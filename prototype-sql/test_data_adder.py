c005870861fdc2d503f70d651f02603cc048ed80
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-05-09 16:50:08 -0400

    Learning how to iterate over rows

--- prototype-sql/test_data_adder.py
+++ prototype-sql/test_data_adder.py
@@ -1,6 +1,8 @@
 import prototype_sql as pt
 import pandas as pd
 
-df = pd.read_csv("/Users/gustavo/Development/SIMSSA-database/prototype-sql/test-data/Josquin\ +\ La\ Rue\ Mass\ duos\ Inventory\ -\ Sheet1.csv")
+df = pd.read_csv("/Users/gustavo/Development/SIMSSA-database/test-data/Josquin + La Rue Mass duos Inventory - Sheet1.csv")
 
-print(df[['Composer', 'Mass']])
+
+for index, row in df.iterrows():
+    print(index, row['Composer'], row['Mass'])
