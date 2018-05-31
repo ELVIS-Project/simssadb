cdcb8ad9a7425fc93f8e219c1594c9ed7e25484c
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-05-31 15:38:14 -0400

    New: Added Profile model that extends User

--- database/models.py
+++ database/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    institution = models.TextField(max_length=100, blank=True)
+    title = models.TextField(max_length=100, blank=True)
