d30c96e41bb6d0c0f3a447133c0b376a59c31b69
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-05-31 15:17:13 -0400

    New: Changed database setttings

--- simssadb/settings.py
+++ simssadb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'database.apps.DatabaseConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'simssadb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'simssadb',
+        'USER': 'gustavo',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
