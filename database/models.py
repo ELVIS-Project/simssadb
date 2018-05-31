56607be9f3bc3304e63f9b8adc1e7f7de3de284e
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-05-31 15:41:23 -0400

    New: Create user profile function when adding a new user

--- database/models.py
+++ database/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -8,3 +10,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     institution = models.TextField(max_length=100, blank=True)
     title = models.TextField(max_length=100, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
