8c7c2a5afdf305d7ae2a87f857d0f52449814c35
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-05 16:38:55 -0400

    New: Parts can be part of a section
    Refs #25

--- database/models/section.py
+++ database/models/section.py
@@ -2,14 +2,15 @@ from django.db import models
 from database.models.custom_base_model import CustomBaseModel
 from database.models.musical_instance import MusicalInstance
 from django.contrib.contenttypes.fields import GenericRelation
+from database.models.part import Part
 
 
 class Section(CustomBaseModel):
     title = models.CharField(max_length=200)
     ordering = models.PositiveIntegerField()
     instance = GenericRelation(MusicalInstance)
-    part_of = models.ForeignKey('self', on_delete=models.CASCADE)
-
+    section_of = models.ForeignKey('self', on_delete=models.CASCADE)
+    has_part = models.ForeignKey(Part, on_delete=models.SET_NULL)
 
     class Meta(CustomBaseModel.Meta):
         db_table = 'section'
