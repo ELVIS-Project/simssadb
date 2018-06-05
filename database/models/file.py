6b1aad841a57986de976a06563961c4493dcb1fe
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-05 16:57:02 -0400

    New: relationships for Musical Instance
    Refs #25

--- database/models/file.py
+++ database/models/file.py
@@ -1,5 +1,6 @@
 from django.db import models
 from database.models.custom_base_model import CustomBaseModel
+from database.models.musical_instance import MusicalInstance
 
 
 class File(CustomBaseModel):
@@ -7,7 +8,8 @@ class File(CustomBaseModel):
     file_size = models.PositiveIntegerField()
     version = models.CharField(max_length=20, null=True)
     encoding_date = models.DateTimeField(null=True)
-
+    manifests = models.ForeignKey(MusicalInstance, related_name='manifested_by',
+                                  on_delete=models.CASCADE)
 
     class Meta:
         abstract = True
