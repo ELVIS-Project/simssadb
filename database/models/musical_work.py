57846a9447375a3aa7e83539eb8f8907055edea4
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-05 14:25:28 -0400

    New: Musical work has relationships to genre, section and part
    Refs #25

--- database/models/musical_work.py
+++ database/models/musical_work.py
@@ -1,18 +1,22 @@
 from django.db import models
 from database.models.custom_base_model import CustomBaseModel
 from django.contrib.postgres.fields import ArrayField
+from database.models.genre import Genre
+from database.models.section import Section
+from database.models.part import Part
 
 
 class MusicalWork(CustomBaseModel):
     title = models.CharField(max_length=200, blank=False)
-    alternative_titles = ArrayField(
+    variant_titles = ArrayField(
             ArrayField(
                     models.CharField(max_length=200, blank=True)
             ),
             blank=True
     )
-    subtitle = models.CharField(max_length=200, blank=True)
-    opus = models.CharField(max_length=200, blank=True)
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
+    sections = models.ManyToManyField(Section)
+    parts = models.ManyToManyField(Part)
 
 
     class Meta(CustomBaseModel.Meta):
