1aa5a6cd06ab979d527eab181983751ae3af85e2
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-06 09:56:16 -0400

    Fixed: Removed date performed from experimental study Fixed: Changed link field to an URL field
    Refs #25

--- database/models/experimental_study.py
+++ database/models/experimental_study.py
@@ -4,10 +4,9 @@ from database.models.custom_base_model import CustomBaseModel
 
 class ExperimentalStudy(CustomBaseModel):
     title = models.CharField(max_length=200, blank=False)
-    date_performed = models.DateField(null=True)
     published = models.BooleanField(default=False)
     date_published = models.DateField(null=True)
-    link = models.CharField(max_length=200, blank=True)
+    link = models.URLField(blank=True)
 
 
     class Meta(CustomBaseModel.Meta):
