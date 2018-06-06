28c670bb8b4da6c92d0cde06f5cfa70d458ff0da
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-06 09:53:18 -0400

    New: Relationships for Research Corpus
    Refs #25

--- database/models/research_corpus.py
+++ database/models/research_corpus.py
@@ -1,10 +1,15 @@
 from django.db import models
 from database.models.custom_base_model import CustomBaseModel
+from database.models.extracted_feature import ExtractedFeature
+from django.contrib.auth.models import User
 
 
 class ResearchCorpus(CustomBaseModel):
     title = models.CharField(max_length=200, blank=False)
-
+    contains = models.ManyToManyField(ExtractedFeature)
+    creators = models.ManyToManyField(User)
+    curators = models.ManyToManyField(User)
+    
 
     class Meta(CustomBaseModel.Meta):
         db_table = 'research_corpus'
