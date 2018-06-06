690e57429f0f02afe7edf5faaad0f91d992ec8e6
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-06 10:10:00 -0400

    New: Relationships for Experimental Study
    Refs #25

--- database/models/experimental_study.py
+++ database/models/experimental_study.py
@@ -1,5 +1,8 @@
 from django.db import models
 from database.models.custom_base_model import CustomBaseModel
+from database.models.research_corpus import ResearchCorpus
+from database.models.institution import Institution
+from django.contrib.auth.models import User
 
 
 class ExperimentalStudy(CustomBaseModel):
@@ -7,7 +10,9 @@ class ExperimentalStudy(CustomBaseModel):
     published = models.BooleanField(default=False)
     date_published = models.DateField(null=True)
     link = models.URLField(blank=True)
-
+    research_corpus_used = models.ForeignKey(ResearchCorpus, on_delete=models.SET_NULL, null=True)
+    institution = models.ForeignKey(Institution, on_delete=models.SET_NULL, null=True)
+    authors = models.ManyToManyField(User)
 
     class Meta(CustomBaseModel.Meta):
         db_table = 'experimental_study'
