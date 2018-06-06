4baad2400e6b467632606fa01d8ee6b962bfe80a
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-06 09:37:33 -0400

    New: Related name for source <-> musical instance relationship
    Refs #25

--- database/models/musical_instance.py
+++ database/models/musical_instance.py
@@ -15,7 +15,7 @@ class MusicalInstance(CustomBaseModel):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to=limit)
     object_id = models.PositiveIntegerField()
     instance_of = GenericForeignKey('content_type', 'object_id')
-    source = models.OneToOneField(Source, on_delete=models.CASCADE)
+    source = models.OneToOneField(Source, on_delete=models.CASCADE, related_name='source_of')
 
     class Meta(CustomBaseModel.Meta):
         db_table = 'musical_instance'
