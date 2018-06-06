8b18fab6ddbbfc667c02958a5d2c496337eb701e
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-06 10:29:36 -0400

    New: Relationships for Source
    Refs #25

--- database/models/source.py
+++ database/models/source.py
@@ -1,5 +1,9 @@
 from django.db import models
 from database.models.custom_base_model import CustomBaseModel
+from database.models.encoder import Encoder
+from database.models.validator import Validator
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class Source(CustomBaseModel):
@@ -9,12 +13,26 @@ class Source(CustomBaseModel):
 
     PHYSICAL = 'p'
     ELECTRONIC = 'e'
-    PHYSICAL_OR_ELECTRONIC_CHOICES = (
+    PHYSICAL_OR_ELECTRONIC = (
         (PHYSICAL, 'Physical'),
         (ELECTRONIC, 'Electronic')
     )
-    physical_or_electronic = models.CharField(max_length=1, choices=PHYSICAL_OR_ELECTRONIC_CHOICES,
+    physical_or_electronic = models.CharField(max_length=1,
+                                              choices=PHYSICAL_OR_ELECTRONIC,
                                               default=PHYSICAL)
+    encoded_by = models.ForeignKey(Encoder, on_delete=models.PROTECT,
+                                   null=False)
+    validator = models.ForeignKey(Validator, on_delete=models.PROTECT,
+                                  null=False)
+
+    # Source can be published by a person or institution
+    limit = models.Q(app_label='database', model='person') | \
+            models.Q(app_label='database', model='institution')
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
+                                     limit_choices_to=limit)
+    object_id = models.PositiveIntegerField()
+    publisher = GenericForeignKey('content_type', 'object_id')
 
 
     class Meta(CustomBaseModel.Meta):
