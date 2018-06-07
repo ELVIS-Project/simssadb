2afe3fb7da1ac917f7701ed98b48561da04b41f9
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-07 13:35:29 -0400

    New: Registering models in admin

--- database/admin.py
+++ database/admin.py
@@ -18,7 +18,11 @@ from database.models.person import Person
 from database.models.institution import Institution
 from database.models.archive import Archive
 from database.models.collection_of_sources import CollectionOfSources
-
+from database.models.contributed_to import ContributedTo
+from database.models.encoder import Encoder
+from database.models.file_in_research_corpus import FileInResearchCorpus
+from database.models.software import Software
+from database.models.validator import Validator
 # Register your models here.
 
 admin.site.register(Profile)
@@ -40,3 +44,8 @@ admin.site.register(Person)
 admin.site.register(Institution)
 admin.site.register(Archive)
 admin.site.register(CollectionOfSources)
+admin.site.register(ContributedTo)
+admin.site.register(Encoder)
+admin.site.register(FileInResearchCorpus)
+admin.site.register(Software)
+admin.site.register(Validator)
