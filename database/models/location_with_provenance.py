eba5e1733e21d847f9e6ee3b959799a2688995a0
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-08 12:23:18 -0400

    New: Model for Location With Provenance

---
+++ database/models/location_with_provenance.py
@@ -0,0 +1,17 @@
+from django.db import models
+from database.models.custom_base_model import CustomBaseModel
+from database.models.geographic_area import GeographicArea
+
+
+class LocationWithProvenance(CustomBaseModel):
+
+    location = models.ForeignKey(GeographicArea)
+    provenance = models.CharField(max_length=200, null=False, blank=True,
+                                  help_text="This is where you enter where "
+                                            "you got the information from")
+
+    def __str__(self):
+        return "{0}:{1}".format(self.location, self.provenance)
+
+    class Meta(CustomBaseModel.Meta):
+        db_table = 'location_with_provenance'
