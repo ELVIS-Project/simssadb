05817f9a92e2320fba5ad0fbc97e8f7420bc41f1
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-08 16:23:26 -0400

    Fixed: Foreign key was missing parameter

--- database/models/location_with_provenance.py
+++ database/models/location_with_provenance.py
@@ -5,7 +5,7 @@ from database.models.geographic_area import GeographicArea
 
 class LocationWithProvenance(CustomBaseModel):
 
-    location = models.ForeignKey(GeographicArea)
+    location = models.ForeignKey(GeographicArea, on_delete=models.PROTECT)
     provenance = models.CharField(max_length=200, null=False, blank=True,
                                   help_text="This is where you enter where "
                                             "you got the information from")
