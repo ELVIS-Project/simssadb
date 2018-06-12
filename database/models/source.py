50925379b7fdeb0e1953b551f0a26f01ca2e1a2e
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-12 16:28:58 -0400

    New: Removed Generic Foreign Key from Source
    Moved the relationship to Collection to here. A source must be part of
    one and only one collection
    Also removed the publication date and publisher because they will be
    in Collection of Sources
    
    Refs #40, #33

--- database/models/source.py
+++ database/models/source.py
@@ -3,21 +3,16 @@ from database.models.custom_base_model import CustomBaseModel
 from database.models.musical_work import MusicalWork
 from database.models.section import Section
 from database.models.part import Part
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
 from django.contrib.postgres.fields import ArrayField
+from database.models.collection_of_sources import CollectionOfSources
 
 
 class Source(CustomBaseModel):
     """Represents a document containing the music defining a specific Instance
 
-    An Instance in turn corresponds to all or part of a Musical Work.
-    Can be divided into Pages.
     Can be a component of a Collection of Sources.
     """
     title = models.CharField(max_length=200, blank=False)
-    publication_date = models.DateField
-    editorial_notes = models.TextField()
     languages = ArrayField(
             ArrayField(
                     models.CharField(max_length=200, blank=True)
@@ -34,23 +29,16 @@ class Source(CustomBaseModel):
     physical_or_electronic = models.CharField(max_length=1,
                                               choices=PHYSICAL_OR_ELECTRONIC,
                                               default=PHYSICAL)
-
-    # Source can be published by a person or institution
-    limit = models.Q(app_label='database', model='person') | models.Q(
-            app_label='database', model='institution')
-
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
-                                     limit_choices_to=limit)
-    object_id = models.PositiveIntegerField()
-    publisher = GenericForeignKey('content_type', 'object_id')
-
     work = models.ManyToManyField(MusicalWork)
     section = models.ManyToManyField(Section)
     part = models.ManyToManyField(Part)
-
+    part_of_collection = models.ForeignKey(CollectionOfSources, null=False,
+                                           blank=False,
+                                           on_delete=models.PROTECT)
 
     def __str__(self):
         return "{0}".format(self.title)
+
 
     class Meta(CustomBaseModel.Meta):
         db_table = 'source'
