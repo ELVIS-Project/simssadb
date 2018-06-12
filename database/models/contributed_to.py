d37e2c85e42be34e0fcfbc38f8eb8e22118275e4
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-12 14:17:58 -0400

    New: Removed Generic Foreign Key from Contributed To relationship
    The table now has one column for each one of Work/Section/Part
    Overrode the clean method to check that only one of the three columns
    is not null and also that not all three are null
    Overrode the save function so it calls the new clean method
    Added constraints at the database level using the django_db_constraints
    plugin
    Also changed the __str__ function to get the one that is not null

--- database/models/contributed_to.py
+++ database/models/contributed_to.py
@@ -1,10 +1,12 @@
 from django.db import models
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
 from database.models.custom_base_model import CustomBaseModel
-from database.models.person import Person
 from django.contrib.postgres.fields import DateRangeField
+from django.core.exceptions import ValidationError
+from database.models.person import Person
 from database.models.geographic_area import GeographicArea
+from database.models.musical_work import MusicalWork
+from database.models.section import Section
+from database.models.part import Part
 
 
 class ContributedTo(CustomBaseModel):
@@ -13,33 +15,82 @@ class ContributedTo(CustomBaseModel):
     A work/section/part can have many contributors with different roles
     i.e. a person composed a piece, two others arranged it, another wrote the
     lyrics
-    A person can be related to a work, section or part, this is implemented
-    using GenericForeignKeys
+    A person can be related to a work, section or part.
     """
     person = models.ForeignKey(Person, on_delete=models.PROTECT)
     certain = models.BooleanField(default=True, null=False, blank=False)
     role = models.CharField(default="Composer", max_length=30)
-    date_of_contrib = DateRangeField(null=True)
+    date = DateRangeField(null=True)
     location = models.ForeignKey(GeographicArea, on_delete=models.SET_NULL,
                                  null=True, blank=True)
 
-    # TODO: Remove this generic foreign key
-    # Generic foreign key to allow polymorphic relation to work/section/part
-    limit = models.Q(app_label='database', model='musicalwork') | models.Q(
-            app_label='database', model='section') | models.Q(
-            app_label='database', model='part')
-
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
-                                     limit_choices_to=limit)
-    object_id = models.PositiveIntegerField()
-    contributed_to = GenericForeignKey('content_type', 'object_id')
-
+    contributed_to_part = models.OneToOneField(Part, null=True,
+                                               blank=True,
+                                               on_delete=models.CASCADE)
+    contributed_to_section = models.OneToOneField(Section, null=True,
+                                                  blank=True,
+                                                  on_delete=models.CASCADE)
+    contributed_to_work = models.OneToOneField(MusicalWork, null=True,
+                                               blank=True,
+                                               on_delete=models.CASCADE)
 
     def __str__(self):
-        return "{0}, {1} of {2}".format(self.person, self.role,
-                                        self.contributed_to)
+        if self.contributed_to_part_id is not None:
+            return "{0}, {1} of {2}".format(self.person, self.role,
+                                            self.contributed_to_part)
+        if self.contributed_to_section_id is not None:
+            return "{0}, {1} of {2}".format(self.person, self.role,
+                                            self.contributed_to_section)
+        if self.contributed_to_work_id is not None:
+            return "{0}, {1} of {2}".format(self.person, self.role,
+                                            self.contributed_to_work)
+        raise AssertionError("Neither 'contributed_to_part', "
+                             "'contributed_to_work' or "
+                             "'contributed_to_section' are set")
 
+    def clean(self):
+        if self.contributed_to_part_id is not None:
+            if self.contributed_to_section_id is not None or \
+                    self.contributed_to_work_id is not None:
+                raise ValidationError('Only one of Work, Section or '
+                                      'Part must be not null')
+        if self.contributed_to_section_id is not None:
+            if self.contributed_to_part_id is not None or \
+                    self.contributed_to_work_id is not None:
+                raise ValidationError('Only one of Work, Section or '
+                                      'Part must be not null')
+        if self.contributed_to_work_id is not None:
+            if self.contributed_to_part_id is not None or \
+                    self.contributed_to_section_id is not None:
+                raise ValidationError('Only one of Work, Section or '
+                                      'Part must be not null')
+        if self.contributed_to_section_id is None and \
+                self.contributed_to_part_id is None and \
+                self.contributed_to_work_id is None:
+            raise ValidationError('At least one of Work, Section or Part '
+                                  'must be not null')
+        super(CustomBaseModel, self).clean()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(CustomBaseModel, self).save()
 
     class Meta(CustomBaseModel.Meta):
         db_table = 'contributed_to'
         verbose_name_plural = 'Contributed To Relationships'
+        db_constraints = {
+            'at_least_one_is_not_null': 'check (contributed_to_section_id is '
+                                        'not null or contributed_to_part_id '
+                                        'is not null or '
+                                        'contributed_to_work_id is not null)',
+            'work_unique': 'check (NOT (contributed_to_work_id is not null '
+                           'and (contributed_to_section_id is not null or '
+                           'contributed_to_part_id is not null)))',
+            'section_unique': 'check (NOT (contributed_to_section_id is not '
+                              'null '
+                           'and (contributed_to_work_id is not null or '
+                           'contributed_to_part_id is not null)))',
+            'part_unique': 'check (NOT (contributed_to_part_id is not null '
+                           'and (contributed_to_section_id is not null or '
+                           'contributed_to_work_id is not null)))'
+        }
