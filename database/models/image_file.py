0e7dcb5f74c0bd53600878c38b39c6ea21d7a84a
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-13 10:21:35 -0400

    New: Updated ImageFile model to handle multiple images and have pages
    Refs #40

--- database/models/image_file.py
+++ database/models/image_file.py
@@ -1,34 +1,26 @@
 from django.db import models
 from database.models.file import File
 from database.models.source import Source
-import os
+from django.contrib.postgres.fields import ArrayField
 
 
 class ImageFile(File):
-    """A manifestation of a Musical Instance as a digital image
-
-    An image file must be an image of one or more Pages from a Source
-    """
-    color_mode = models.CharField(max_length=20)
-    compression_type = models.CharField(max_length=20)
-    gama_correction = models.CharField(max_length=20)
-    color_calibration = models.CharField(max_length=20)
-    pixel_width = models.PositiveIntegerField()
-    pixel_height = models.PositiveIntegerField()
-    ppi = models.PositiveIntegerField()
-
+    """A manifestation of a Source as digital images"""
     manifests = models.ForeignKey(Source,
                                   related_name='manifested_by_image_file',
                                   on_delete=models.CASCADE, null=False)
 
-    # Should we change this to image field?
+    files = ArrayField(models.ImageField(upload_to='images/'),
+                       null=False, blank=False)
 
-    file = models.FileField(upload_to='images/')
+    title = models.CharField(max_length=100, null=True, blank=True)
 
+    @property
+    def pages(self):
+        return self.files.__len__()
 
     def __str__(self):
-        filename = os.path.basename(self.file.name)
-        return "{0}".format(filename)
+        return "Images of {0}".format(self.manifests)
 
     class Meta:
         db_table = 'image_file'
