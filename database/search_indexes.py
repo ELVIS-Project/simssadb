7bb43c7e3cdf6b3c1feffba5cf48a5b86a3aec7b
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-15 12:30:57 -0400

    New: Index and index template for Musical Work
    Refs #30

--- database/search_indexes.py
+++ database/search_indexes.py
@@ -5,11 +5,11 @@ from database.models.person import Person
 from database.models.institution import Institution
 from database.models.instrument import Instrument
 from database.models.genre import Genre
+from database.models.musical_work import MusicalWork
 
 
 class InstrumentIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    content_auto = indexes.EdgeNgramField(model_attr='name')
 
     def get_model(self):
         return Instrument
@@ -17,7 +17,6 @@ class InstrumentIndex(indexes.SearchIndex, indexes.Indexable):
 
 class GenreIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    content_auto = indexes.EdgeNgramField(model_attr='name')
 
     def get_model(self):
         return Genre
@@ -25,7 +24,6 @@ class GenreIndex(indexes.SearchIndex, indexes.Indexable):
 
 class InstitutionIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    content_auto = indexes.EdgeNgramField(model_attr='name')
     url = indexes.CharField(model_attr='website')
 
     def get_model(self):
@@ -44,3 +42,14 @@ class GeographicAreaIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return GeographicArea
+
+
+class MusicalWorkIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    # style = indexes.MultiValueField(model_attr='genres_as_in_style')
+    # form = indexes.MultiValueField(model_attr='genres_as_in_form')
+    religiosity = indexes.BooleanField(model_attr='religiosity')
+    # composers = indexes.MultiValueField(model_attr='composers__name')
+
+    def get_model(self):
+        return MusicalWork
