1c0d3f26aac50f81851fe24aed6b1520f3fd5b26
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-18 10:47:15 -0400

    New: Added composers field to Musical Work serializer

--- database/serializers.py
+++ database/serializers.py
@@ -41,9 +41,15 @@ class SectionSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class MusicalWorkSerializer(serializers.HyperlinkedModelSerializer):
+    composers = serializers.HyperlinkedRelatedField(
+            many=True,
+            read_only=True,
+            view_name='person-detail'
+    )
+
     class Meta:
         model = MusicalWork
         fields = ('variant_titles', 'genres_as_in_style', 'genres_as_in_form',
                   'sections', 'religiosity', 'viaf_url',
-                  'other_authority_control_url', 'contributors')
-
+                  'other_authority_control_url', 'contributors',
+                  'composers')
