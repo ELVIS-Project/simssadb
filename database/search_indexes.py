df522e6449d1b3096a38f9f1c48bfee8f7d15051
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-18 14:45:22 -0400

    New: Added search index for sections

--- database/search_indexes.py
+++ database/search_indexes.py
@@ -1,4 +1,3 @@
-import datetime
 from haystack import indexes
 from database.models.geographic_area import GeographicArea
 from database.models.person import Person
@@ -6,6 +5,7 @@ from database.models.institution import Institution
 from database.models.instrument import Instrument
 from database.models.genre import Genre
 from database.models.musical_work import MusicalWork
+from database.models.section import Section
 
 
 class InstrumentIndex(indexes.SearchIndex, indexes.Indexable):
@@ -46,10 +46,14 @@ class GeographicAreaIndex(indexes.SearchIndex, indexes.Indexable):
 
 class MusicalWorkIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    # style = indexes.MultiValueField(model_attr='genres_as_in_style')
-    # form = indexes.MultiValueField(model_attr='genres_as_in_form')
     religiosity = indexes.BooleanField(model_attr='religiosity')
-    # composers = indexes.MultiValueField(model_attr='composers__name')
 
     def get_model(self):
         return MusicalWork
+
+
+class SectionIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Section
