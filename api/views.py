8f5245d17875a7d3b9850d9e0a2c54a0e1fded2c
Andrew Kam <andrew.kam@outlook.com>
2018-06-19 16:38:38 -0400

    Fixed: Changed VIAF search to use auto suggest retrieval for more accurate results
    refs #44

--- api/views.py
+++ api/views.py
@@ -9,24 +9,21 @@ def ViafComposerSearch(request):
         """Return JSON with suggested VIAF ids and display names."""
         viaf = ViafAPI()
 
-        result = viaf.find_person(value)
+        result = viaf.suggest(value)
 
         # check for empty search result and return empty json response
         if result is None:
             return JsonResponse({'results': []})
 
+        result = [item for item in result
+           if item['nametype'] == 'personal']
+
         return JsonResponse({
             'results': [dict(
-                # id=viaf.uri_from_id(item.recordData.viafID),
-                id=item.uri,
-                id_number=item.viaf_id,
-                text=item.label,
-                nametype=item.nametype,
-                # possibly useful to include, since we have them (for people)
-                birth=item.recordData.birthDate,
-                death=item.recordData.deathDate,
-            # exclude any names that are not personal
-            ) for item in result if value.lower() in item.label.lower()]
+                uri=viaf.uri_from_id(item['viafid']),
+                id=item['viafid'],
+                text=item['displayForm'],
+            ) for item in result]
         })
 
 def WikidataComposerSearch(request):
@@ -55,7 +52,7 @@ def WikidataComposerSearch(request):
 
         return JsonResponse({
             'results': [dict(
-                id=item["item"]["value"],
+                uri=item["item"]["value"],
                 text=item["label"]["value"],
                 # birth=item["date_of_birth"]["value"],
                 # death=item["date_of_death"]["value"],
