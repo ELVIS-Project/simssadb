f596bf8d68e49fa4b3560ca0b1e8034d3dc7d692
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2018-06-26 14:50:47 -0400

    Fix login issue

--- database/views/front_end_views.py
+++ database/views/front_end_views.py
@@ -6,7 +6,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
-from .tokens import account_activation_token
+from database.tokens import account_activation_token
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.core.mail import EmailMessage
