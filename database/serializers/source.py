6a6e0dba01d5d9a11aee99e8d017260de581d551
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-26 10:51:13 -0400

    Formatting

--- database/serializers/source.py
+++ database/serializers/source.py
@@ -6,8 +6,10 @@ from rest_framework_recursive.fields import RecursiveField
 
 class SourceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        parent_sources = RecursiveField(required=False, allow_null=True, many=True)
-        child_sources = RecursiveField(required=False, allow_null=True, many=True)
+        parent_sources = RecursiveField(required=False, allow_null=True,
+                                        many=True)
+        child_sources = RecursiveField(required=False, allow_null=True,
+                                       many=True)
         model = Source
         fields = ('url', 'title', 'languages',
                   'work', 'section', 'part', 'part_of_collection')
