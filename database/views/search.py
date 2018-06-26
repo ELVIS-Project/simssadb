276d40febff5297b279e4b9d299463f8da7750bc
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-26 14:01:05 -0400

    New: Updated search to filter by models
    Also more robust when getting the url parameters
    
    Refs #30

--- database/views/search.py
+++ database/views/search.py
@@ -1,18 +1,28 @@
 from haystack.query import SearchQuerySet
 from haystack.generic_views import SearchView
+from django.apps import apps
 
 
 class GeneralSearch(SearchView):
     template_name = 'search/general-search.html'
 
     # TODO: Make this more robust in terms of getting parameters from the URL
-    # TODO: Enable filtering by model
     def get_queryset(self):
         queryset = super(GeneralSearch, self).get_queryset()
         if self.request.method == 'GET':
-            params = self.request.GET.dict()
-            if params:
+            params = self.request.GET
+            if params.getlist('q') and not params.getlist('models',
+                                                          default=False):
                 queryset = SearchQuerySet().filter(text__fuzzy=params['q'])
+            if params.getlist('models'):
+                models = []
+                for item in params.getlist('models'):
+                    app_label, model_name = item.split('.')
+                    model = apps.get_model(app_label=app_label,
+                                           model_name=model_name)
+                    models.append(model)
+                queryset = SearchQuerySet().models(*models).filter(
+                        text__fuzzy=params['q'])
         return queryset
 
     def get_context_data(self, *args, **kwargs):
