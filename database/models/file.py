9e9ad47feb7fa39bd6948b3a5c0c3e3288cb4b3a
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-27 16:43:21 -0400

    New: help-text for all fields in File
    Refs #60
    
    Signed-off-by: Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>

--- database/models/file.py
+++ database/models/file.py
@@ -10,15 +10,26 @@ class File(CustomBaseModel):
 
     Most if not all fields should be extracted automatically
     """
-    file_type = models.CharField(max_length=10)
-    file_size = models.PositiveIntegerField()
-    version = models.CharField(max_length=20, null=True)
-    encoding_date = models.DateTimeField(null=True)
+    file_type = models.CharField(max_length=10, help_text='The format of the '
+                                                          'File')
+    file_size = models.PositiveIntegerField(null=True, blank=True,
+                                            help_text='The size of the File '
+                                                      'in bytes')
+    version = models.CharField(max_length=20, null=True,
+                               help_text='The version of the encoding schema '
+                                         '(i.e. MEI 2.0)')
+    encoding_date = models.DateTimeField(null=True,
+                                         help_text='The date the File was '
+                                                   'encoded')
     encoded_with = models.ForeignKey(Encoder, on_delete=models.PROTECT,
-                                     null=False)
+                                     null=False, help_text='The Encoder of '
+                                                           'this File')
     validated_by = models.ForeignKey(Validator, on_delete=models.SET_NULL,
-                                     null=True, blank=True)
-    extra_metadata = JSONField(null=True, blank=True)
+                                     null=True, blank=True,
+                                     help_text='The Validator of this file')
+    extra_metadata = JSONField(null=True, blank=True,
+                               help_text='Any extra metadata associated with '
+                                         'the File')
 
     class Meta(CustomBaseModel.Meta):
         abstract = True
