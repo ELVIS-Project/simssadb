e13cbde92af589e4c7c88d6735b02956e3715bc8
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-27 11:24:39 -0400

    New: Part now only belongs to one and only one Section
    Refs #40
    
    Signed-off-by: Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>

--- database/models/part.py
+++ database/models/part.py
@@ -1,6 +1,7 @@
 from django.db import models
 from database.models.custom_base_model import CustomBaseModel
 from database.models.instrument import Instrument
+from database.models.section import Section
 
 
 class Part(CustomBaseModel):
@@ -12,6 +13,8 @@ class Part(CustomBaseModel):
     label = models.CharField(max_length=200)
     written_for = models.ManyToManyField(Instrument,
                                          related_name='part_written_for')
+    in_section = models.ForeignKey(Section, on_delete=models.CASCADE,
+                                   related_name='parts')
     contributors = models.ManyToManyField(
             'Person',
             through='ContributedTo',
