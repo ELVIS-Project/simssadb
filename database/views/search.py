162c449fbcbd91566347a323aa611d1f07cf281f
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-27 09:18:59 -0400

    New: Changed the search to only allow one field, not specifying model
    The search now looks into all models, results are separated by model.
    Eventually we will add faceting and more advanced search features.
    
    Refs #30

--- database/views/search.py
+++ database/views/search.py
@@ -1,28 +1,18 @@
 from haystack.query import SearchQuerySet
 from haystack.generic_views import SearchView
-from django.apps import apps
+from haystack.forms import SearchForm
 
 
 class GeneralSearch(SearchView):
     template_name = 'search/general-search.html'
+    form_class = SearchForm
 
-    # TODO: Make this more robust in terms of getting parameters from the URL
     def get_queryset(self):
         queryset = super(GeneralSearch, self).get_queryset()
         if self.request.method == 'GET':
             params = self.request.GET
-            if params.getlist('q') and not params.getlist('models',
-                                                          default=False):
+            if params.get('q', default=False):
                 queryset = SearchQuerySet().filter(text__fuzzy=params['q'])
-            if params.getlist('models'):
-                models = []
-                for item in params.getlist('models'):
-                    app_label, model_name = item.split('.')
-                    model = apps.get_model(app_label=app_label,
-                                           model_name=model_name)
-                    models.append(model)
-                queryset = SearchQuerySet().models(*models).filter(
-                        text__fuzzy=params['q'])
         return queryset
 
     def get_context_data(self, *args, **kwargs):
