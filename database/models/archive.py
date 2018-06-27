5dbe41dabb3d843b7ba70bfe2267b007e91ef71a
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-27 14:22:51 -0400

    New: help-text for all fields in Archive
    Refs #60
    
    Signed-off-by: Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>

--- database/models/archive.py
+++ database/models/archive.py
@@ -10,12 +10,17 @@ class Archive(CustomBaseModel):
 
     e.g: A database or a library
     """
-    name = models.CharField(max_length=200, blank=False, null=False)
+    name = models.CharField(max_length=200, blank=False, null=False,
+                            help_text='The name of the Archive')
     collections = models.ManyToManyField(CollectionOfSources,
-                                         related_name='in_archive')
+                                         related_name='in_archive',
+                                         help_text='Sources that belong '
+                                                   'to this Archive')
 
     institution = models.ForeignKey(Institution, null=True,
-                                    on_delete=models.SET_NULL)
+                                    on_delete=models.SET_NULL,
+                                    help_text='The Institution that this '
+                                              'Archive is part of')
 
     def __str__(self):
         return "{0}".format(self.name)
