aa353ecd3760147106f499241bf059302e3f0d96
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-28 15:43:21 -0400

    New: help-text for all fields in Source
    Refs #60
    
    Signed-off-by: Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>

--- database/models/source.py
+++ database/models/source.py
@@ -13,19 +13,40 @@ class Source(CustomBaseModel):
     Must be part of a Collection of Sources.
     """
     languages = ArrayField(models.CharField(max_length=200, blank=True),
-                           blank=True, null=True)
+                           blank=True, null=True,
+                           help_text='The languages this Source is written in')
     work = models.ForeignKey(MusicalWork, null=False, blank=False,
-                             on_delete=models.PROTECT)
-    sections = models.ManyToManyField(Section)
-    parts = models.ManyToManyField(Part)
+                             on_delete=models.PROTECT,
+                             help_text='The Musical Work manifested in part '
+                                       'or in full by this Source')
+    sections = models.ManyToManyField(Section,
+                                      help_text='The Section or Sections '
+                                                'manifested in full by this '
+                                                'Source')
+    parts = models.ManyToManyField(Part,
+                                   help_text='The Part or Parts '
+                                             'manifested in full by this '
+                                             'Source')
     part_of_collection = models.ForeignKey(CollectionOfSources, null=False,
                                            blank=False,
-                                           on_delete=models.PROTECT)
+                                           on_delete=models.PROTECT,
+                                           help_text='The Collection of '
+                                                     'Sources this Source '
+                                                     'belongs to')
     parent_sources = models.ManyToManyField('self',
                                             related_name='child_sources',
-                                            blank=True)
-    portion = models.TextField(max_length=255, blank=True, null=True)
-    url = models.URLField(null=True, blank=True)
+                                            blank=True,
+                                            help_text='The Source this Source '
+                                                      'derives from')
+    portion = models.TextField(max_length=255, blank=True, null=True,
+                               help_text='Specifies which portion of the '
+                                         'Collection of Sources this Source '
+                                         'represents. i.e. a page range, '
+                                         'a number of folios, a number of '
+                                         'files in a database, etc')
+    url = models.URLField(null=True, blank=True,
+                          help_text='An link back to an electronic version of '
+                                    'this Source, if it exists')
 
     def __str__(self):
         return "Part of {0}, source of ".format(self.part_of_collection.title,
