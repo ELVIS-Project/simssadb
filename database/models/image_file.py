dc467347fd21c1fdc9a33d61aeb2731cfa54b4d5
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-28 11:22:21 -0400

    New: help-text for all fields in ImageFile. Also removed title attribute
    Refs #60
    
    Signed-off-by: Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>

--- database/models/image_file.py
+++ database/models/image_file.py
@@ -5,18 +5,24 @@ from django.contrib.postgres.fields import ArrayField
 
 
 class ImageFile(File):
-    """A manifestation of a Source as digital images"""
+    """
+    A manifestation of a Source as digital images
+
+    Generated from a source by an Encoder and can be validate by a Validator
+    """
     manifests = models.ForeignKey(Source,
                                   related_name='manifested_by_image_file',
-                                  on_delete=models.CASCADE, null=False)
+                                  on_delete=models.CASCADE, null=False,
+                                  help_text='The Source manifested by these '
+                                            'images')
 
     files = ArrayField(models.ImageField(upload_to='images/'),
-                       null=False, blank=False)
-
-    title = models.CharField(max_length=100, null=True, blank=True)
+                       null=False, blank=False,
+                       help_text='The actual set of image files')
 
     @property
     def pages(self):
+        """Gets the number of images, which is equal to the number of pages"""
         return self.files.__len__()
 
     def __str__(self):
