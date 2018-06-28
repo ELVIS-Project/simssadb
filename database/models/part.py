9e7314c1b8f8a4b96d879694bd3e6e466428d6d1
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-28 11:49:57 -0400

    New: help-text for all fields in Part
    Refs #60
    
    Signed-off-by: Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>

--- database/models/part.py
+++ database/models/part.py
@@ -11,17 +11,29 @@ class Part(CustomBaseModel):
     Purely abstract entity that can manifest in differing versions.
     Must belong to one and only one Section
     """
-    label = models.CharField(max_length=200)
+    label = models.CharField(max_length=200,
+                             help_text='Any label that could help describe '
+                                       'this Part')
     written_for = models.ManyToManyField(Instrument,
-                                         related_name='part_written_for')
+                                         related_name='part_written_for',
+                                         help_text='The Instrument or Voice '
+                                                   'for which this Part is '
+                                                   'written')
     in_section = models.ForeignKey(Section, on_delete=models.CASCADE,
-                                   related_name='parts')
+                                   related_name='parts',
+                                   help_text='The Section to which this Part '
+                                             'belongs')
     contributors = models.ManyToManyField(
             'Person',
             through='ContributedTo',
             through_fields=(
-                'contributed_to_part', 'person')
-    )
+                'contributed_to_part', 'person'),
+            help_text='All the People that '
+                      'contributed to this '
+                      'Part in different '
+                      'capacities such as '
+                      'composer or arranger'
+            )
 
 
     def __str__(self):
