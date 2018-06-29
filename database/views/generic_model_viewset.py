cd280a3cddcc6484167acf3ec5036d1ba3b770dc
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-06-29 14:10:03 -0400

    New: Removed pagination class, added BrowsableAPIRenderer
    Now every link appended with a .api will show the browsable API

--- database/views/generic_model_viewset.py
+++ database/views/generic_model_viewset.py
@@ -1,8 +1,7 @@
 from rest_framework import viewsets
-from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer, \
+    TemplateHTMLRenderer
 from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
-from rest_framework.pagination import PageNumberPagination
 
 
 class GenericModelViewSet(viewsets.ModelViewSet):
@@ -13,8 +12,8 @@ class GenericModelViewSet(viewsets.ModelViewSet):
     """
 
     # In the future we can add even more renderers to return things like XML
-    renderer_classes = (TemplateHTMLRenderer, JSONRenderer)
-    pagination_class = PageNumberPagination
+    renderer_classes = (TemplateHTMLRenderer, JSONRenderer,
+                        BrowsableAPIRenderer)
 
     def get_base_name(self):
         """Get the base_name that will be used in this view
