8f99d47f395145977319529eaaa6c572758056d0
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-03 15:51:57 -0400

    Fixed: docstrings in the FileAndSourceInfoMixin
    Refs #61

--- database/mixins/file_and_source_info.py
+++ database/mixins/file_and_source_info.py
@@ -6,17 +6,17 @@ class FileAndSourceInfoMixin(object):
 
     @property
     def symbolic_files(self):
-        """Gets all the Symbolic Files related to this Work"""
+        """Gets all the Symbolic Files related to this Work/Section/Part"""
         files = []
         sources = self.sources.all()
         for source in sources:
-            files.append(source.manifested_by_sym_file.all())
+            files.append(list(source.manifested_by_sym_file.all()))
         return files
 
 
     @property
     def symbolic_music_formats(self):
-        """Gets the formats of all the Symbolic Files related to this Work"""
+        """Gets the formats of all the Symbolic Files related to this Work/Section/Part"""
         formats = set()
         files = self.symbolic_files
         for file in files:
@@ -26,17 +26,17 @@ class FileAndSourceInfoMixin(object):
 
     @property
     def image_files(self):
-        """Gets all the Image Files related to this Work"""
+        """Gets all the Image Files related to this Work/Section/Part"""
         files = []
         sources = self.sources.all()
         for source in sources:
-            files.append(source.manifested_by_image_files.all())
+            files.append(list(source.manifested_by_image_files.all()))
         return files
 
 
     @property
     def image_formats(self):
-        """Gets the formats of all the Image Files related to this Work"""
+        """Gets the formats of all the Image Files related to this Work/Section/Part"""
         formats = set()
         files = self.image_files
         for file in files:
@@ -46,17 +46,17 @@ class FileAndSourceInfoMixin(object):
 
     @property
     def text_files(self):
-        """Gets all the Symbolic Files related to this Work"""
+        """Gets all the Symbolic Files related to this Work/Section/Part"""
         files = []
         sources = self.sources.all()
         for source in sources:
-            files.append(source.manifested_by_text_files.all())
+            files.append(list(source.manifested_by_text_files.all()))
         return files
 
 
     @property
     def text_formats(self):
-        """Gets the formats of all the Image Files related to this Work"""
+        """Gets the formats of all the Image Files related to this Work/Section/Part"""
         formats = set()
         files = self.text_files
         for file in files:
@@ -66,17 +66,17 @@ class FileAndSourceInfoMixin(object):
 
     @property
     def audio_files(self):
-        """Gets all the Symbolic Files related to this Work"""
+        """Gets all the Symbolic Files related to this Work/Section/Part"""
         files = []
         sources = self.sources.all()
         for source in sources:
-            files.append(source.manifested_by_audio_files.all())
+            files.append(list(source.manifested_by_audio_files.all()))
         return files
 
 
     @property
     def audio_formats(self):
-        """Gets the formats of all the Image Files related to this Work"""
+        """Gets the formats of all the Image Files related to this Work/Section/Part"""
         formats = set()
         files = self.audio_files
         for file in files:
@@ -86,27 +86,27 @@ class FileAndSourceInfoMixin(object):
 
     @property
     def encoders(self):
-        """Gets all the Encoders for files related to this Musical Work"""
+        """Gets all the Encoders for files related to this Work/Section/Part"""
         encoders = set()
         sources = self.sources.all()
         for source in sources:
-            encoders.update(source.encoders())
+            encoders.update(list(source.encoders.all()))
         return encoders
 
 
     @property
     def validators(self):
-        """Gets all the Validators for files related to this Musical Work"""
+        """Gets all the Validators for files related to this Work/Section/Part"""
         validators = set()
         sources = self.sources.all()
         for source in sources:
-            validators.update(source.validators())
+            validators.update(list(source.validators.all()))
         return validators
 
 
     @property
     def features(self):
-        """Gets all the Features extracted from files related to this Work"""
+        """Gets all the Features extracted from files related to this Work/Section/Part"""
         features = []
         sym_files = self.symbolic_files
         for file in sym_files:
@@ -117,7 +117,7 @@ class FileAndSourceInfoMixin(object):
 
     @property
     def collections_of_sources(self):
-        """Gets all the Collections of Sources related to this Work"""
+        """Gets all the Collections of Sources related to this Work/Section/Part"""
         collections = set()
         sources = self.sources.all()
         for source in sources:
@@ -127,7 +127,7 @@ class FileAndSourceInfoMixin(object):
 
     @property
     def languages(self):
-        """Gets all the languages of the Sources and Text Files related to this Work"""
+        """Gets all the languages of the Sources and Text Files related to this Work/Section/Part"""
         languages = set()
         sources = self.sources.all()
         # This is a bit ugly, but I'm not sure how to do it better
