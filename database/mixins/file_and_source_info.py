1bb7ebe32a0412d451dc41ec07b2e084933fa336
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-03 15:15:35 -0400

    New: mixin for accessing file and source info in Work/Section/Part
    Refs #61

---
+++ database/mixins/file_and_source_info.py
@@ -0,0 +1,140 @@
+class FileAndSourceInfoMixin(object):
+    """
+    A mixin for Work/Section/Part to access information about Sources and Files
+    """
+
+
+    @property
+    def symbolic_files(self):
+        """Gets all the Symbolic Files related to this Work"""
+        files = []
+        sources = self.sources.all()
+        for source in sources:
+            files.append(source.manifested_by_sym_file.all())
+        return files
+
+
+    @property
+    def symbolic_music_formats(self):
+        """Gets the formats of all the Symbolic Files related to this Work"""
+        formats = set()
+        files = self.symbolic_files
+        for file in files:
+            formats.add(file.file_type)
+        return formats
+
+
+    @property
+    def image_files(self):
+        """Gets all the Image Files related to this Work"""
+        files = []
+        sources = self.sources.all()
+        for source in sources:
+            files.append(source.manifested_by_image_files.all())
+        return files
+
+
+    @property
+    def image_formats(self):
+        """Gets the formats of all the Image Files related to this Work"""
+        formats = set()
+        files = self.image_files
+        for file in files:
+            formats.add(file.file_type)
+        return formats
+
+
+    @property
+    def text_files(self):
+        """Gets all the Symbolic Files related to this Work"""
+        files = []
+        sources = self.sources.all()
+        for source in sources:
+            files.append(source.manifested_by_text_files.all())
+        return files
+
+
+    @property
+    def text_formats(self):
+        """Gets the formats of all the Image Files related to this Work"""
+        formats = set()
+        files = self.text_files
+        for file in files:
+            formats.add(file.file_type)
+        return formats
+
+
+    @property
+    def audio_files(self):
+        """Gets all the Symbolic Files related to this Work"""
+        files = []
+        sources = self.sources.all()
+        for source in sources:
+            files.append(source.manifested_by_audio_files.all())
+        return files
+
+
+    @property
+    def audio_formats(self):
+        """Gets the formats of all the Image Files related to this Work"""
+        formats = set()
+        files = self.audio_files
+        for file in files:
+            formats.add(file.file_type)
+        return formats
+
+
+    @property
+    def encoders(self):
+        """Gets all the Encoders for files related to this Musical Work"""
+        encoders = set()
+        sources = self.sources.all()
+        for source in sources:
+            encoders.update(source.encoders())
+        return encoders
+
+
+    @property
+    def validators(self):
+        """Gets all the Validators for files related to this Musical Work"""
+        validators = set()
+        sources = self.sources.all()
+        for source in sources:
+            validators.update(source.validators())
+        return validators
+
+
+    @property
+    def features(self):
+        """Gets all the Features extracted from files related to this Work"""
+        features = []
+        sym_files = self.symbolic_files
+        for file in sym_files:
+            for feature in file.extractedfeature_set.all():
+                features.append(feature)
+        return features
+
+
+    @property
+    def collections_of_sources(self):
+        """Gets all the Collections of Sources related to this Work"""
+        collections = set()
+        sources = self.sources.all()
+        for source in sources:
+            collections.add(source.part_of_collection)
+        return collections
+
+
+    @property
+    def languages(self):
+        """Gets all the languages of the Sources and Text Files related to this Work"""
+        languages = set()
+        sources = self.sources.all()
+        # This is a bit ugly, but I'm not sure how to do it better
+        for source in sources:
+            for language in source.languages:
+                languages.add(language)
+            for text_file in source.manifested_by_text_files.all():
+                for language in text_file.languages:
+                    languages.add(language)
+        return languages
