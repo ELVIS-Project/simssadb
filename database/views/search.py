be30292287a25aa0412e393da6d3da63844dd059
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-10 14:05:55 -0400

    New: faceted search view
    Also added the url config for it
    
    Refs #52

--- database/views/search.py
+++ database/views/search.py
@@ -1,8 +1,19 @@
 from haystack.generic_views import SearchView
-
+from haystack.generic_views import FacetedSearchView
+from haystack.forms import FacetedSearchForm
 from database.forms import FuzzySearchForm
 
 
 class GeneralSearch(SearchView):
     template_name = 'search/general-search.html'
     form_class = FuzzySearchForm
+
+
+class FacetedSearch(FacetedSearchView):
+    facet_fields = ['places', 'dates', 'sym_formats', 'audio_formats',
+                    'text_formats', 'image_formats', 'certainty',
+                    'languages', 'religiosity', 'instruments',
+                    'composers']
+    context_object_name = 'object_list'
+    template_name = 'search/faceted_search.html'
+    form_class = FacetedSearchForm
