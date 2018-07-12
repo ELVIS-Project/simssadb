5f8bd918dbebd63a20ebb2408ed4c3586b66caed
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-12 16:28:35 -0400

    New: added prepare_summary to ContributedTo
    Refs #103

--- database/models/contributed_to.py
+++ database/models/contributed_to.py
@@ -113,8 +113,41 @@ class ContributedTo(CustomBaseModel):
         self.full_clean()
         super(CustomBaseModel, self).save()
 
-    def __prepare_summary(self):
-        pass
+    def __get_date(self):
+        date = None
+        if self.date is not None:
+            if self.date.lower is not None and self.date.upper is not None:
+                if self.date.lower.year == self.date.upper.year:
+                    date = str(self.date.upper.year)
+                else:
+                    date = str(self.date.lower.year) + '-' + str(self.date.upper.year)
+            if self.date.lower is not None and self.date.upper is None:
+                date = str(self.date.lower.year)
+            if self.date.lower is None and self.date.upper is not None:
+                date = str(self.date.upper.year)
+        return date
+
+    def prepare_summary(self):
+        date = self.__get_date()
+
+        if self.location is not None:
+            location = self.location.name
+        else:
+            location = None
+
+        summary = {'display': '',
+                   'url': self.person.get_absolute_url(),
+                   'role': self.role.lower(),
+                   'person': self.person.__str__(),
+                   'date': date,
+                   'location': location,
+                   'certain': self.certain
+                   }
+
+        return summary
+
+    def summary(self):
+        return self.prepare_summary()
 
     class Meta(CustomBaseModel.Meta):
         db_table = 'contributed_to'
