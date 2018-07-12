d1e859b073d63ecb50e4b2a4c660e5d45d7cc3b9
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-12 11:37:29 -0400

    New: __prepare_summary in Genre
    Refs #103

--- database/models/genre.py
+++ database/models/genre.py
@@ -15,5 +15,11 @@ class Genre(CustomBaseModel):
     def __str__(self):
         return "{0}".format(self.name)
 
+    def __prepare_summary(self):
+        summary = {'display': self.__str__(),
+                   'url': self.get_absolute_url()
+                   }
+        return summary
+
     class Meta(CustomBaseModel.Meta):
         db_table = 'genre'
