3d09d76ef80dcd59ad11b6ca2da1d3b9e6d1307a
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-19 10:18:17 -0400

    New: refactored for better query performance
    Uses new contribution_helper functions
    Only loads the query set once
    
    Refs #89

--- database/models/section.py
+++ database/models/section.py
@@ -1,11 +1,10 @@
 from django.db import models
-
-from database.mixins.contributed_to_info_mixin import ContributedToInfoMixin
 from database.mixins.file_and_source_info import FileAndSourceInfoMixin
 from database.models.custom_base_model import CustomBaseModel
+import database.mixins.contribution_helper as contribution_helper
 
 
-class Section(FileAndSourceInfoMixin, ContributedToInfoMixin, CustomBaseModel):
+class Section(FileAndSourceInfoMixin, CustomBaseModel):
     """
     A component of a Musical Work e.g. an Aria in an Opera
 
@@ -46,37 +45,56 @@ class Section(FileAndSourceInfoMixin, ContributedToInfoMixin, CustomBaseModel):
             instruments.add(part.written_for)
         return instruments
 
-    def __composers_for_summary(self):
-        composers = self.composers
+    @staticmethod
+    def _composers_for_summary(composers):
         if len(composers) > 1:
             return composers[0]['person'].__str__() + ' and others'
         else:
             return composers[0]['person'].__str__()
 
-    def __works_for_summary(self):
-        work_count = self.in_works.count()
-        if work_count > 1:
-            return self.in_works.all()[0].__str__() + " and others"
-        else:
-            return self.in_works.all()[0].__str__()
+    @staticmethod
+    def _works_for_summary(works):
+        return works[0].__str__()
 
     def __str__(self):
         return "{0}".format(self.title)
 
-    def __badge_name(self):
-        if self.parts.count() > 1:
+    @staticmethod
+    def _badge_name(parts_count):
+        if parts_count > 1:
             return 'parts'
         else:
             return 'part'
 
+    @property
+    def certainty(self):
+        """Returns True if all the relationships have certain == True"""
+        certainties = self.contributed_to.values_list('certain', flat=True)
+        print(certainties)
+        if False in certainties:
+            return False
+        else:
+            return True
+
     def prepare_summary(self):
+        contributions = self.contributed_to.all().select_related('person')
+        works = self.in_works.all()
+        contributions_summaries = contribution_helper.get_contributions_summaries(contributions)
+        composers = contribution_helper.filter_contributions_by_role(contributions_summaries, 'composer')
+        parts_count = self.parts.count()
+
+        if contribution_helper.dates_of_contribution(composers):
+            date = contribution_helper.dates_of_contribution(composers)[0]
+        else:
+            date = 'Unknown'
+
         summary = {'display': self.__str__(),
                    'url': self.get_absolute_url(),
-                   'composer': self.__composers_for_summary(),
-                   'date': self.dates_of_composition[0],
-                   'badge_name': self.__badge_name(),
-                   'badge_count': self.parts.count(),
-                   'musical work': self.__works_for_summary()
+                   'composer': self._composers_for_summary(composers),
+                   'date': date,
+                   'badge_name': self._badge_name(parts_count),
+                   'badge_count': parts_count,
+                   'musical work': self._works_for_summary(works)
                    }
         return summary
 
