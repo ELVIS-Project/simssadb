e4eaa539410194e166fd6cb0644ad875fa977a46
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-19 10:16:22 -0400

    New: changed prefetch_related to only fetch the necessary things
    Refs #89

--- database/views/section.py
+++ database/views/section.py
@@ -4,6 +4,5 @@ from database.models.section import Section
 
 
 class SectionViewSet(GenericModelViewSet):
-    queryset = Section.objects.all().prefetch_related('contributed_to', 'contributors',
-                                                      'parts', 'parts__written_for')
+    queryset = Section.objects.all().prefetch_related('parts', 'in_works')
     serializer_class = SectionSerializer
