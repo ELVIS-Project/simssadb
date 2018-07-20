b708480c6b85de610f9813d67cbb161aa5d6f5c5
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-20 09:33:53 -0400

    New: forgot to add the sorting by given_name after surname

--- database/views/person.py
+++ database/views/person.py
@@ -7,7 +7,7 @@ from database.serializers import PersonSearchSerializer
 
 class PersonViewSet(GenericModelViewSet):
     queryset = Person.objects.all().prefetch_related('works_contributed_to',
-                                                     'sections_contributed_to').order_by('surname')
+                                                     'sections_contributed_to').order_by('surname', 'given_name')
     serializer_class = PersonSerializer
 
 
