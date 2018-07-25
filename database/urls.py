a484f90657c3f4fdd12eafe8561894c503771750
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-25 14:52:58 -0400

    New: registered extractedfeatures and encoders with the url router

--- database/urls.py
+++ database/urls.py
@@ -17,6 +17,8 @@ router.register(r'sources', views.SourceViewSet)
 router.register(r'collections', views.CollectionOfSourcesViewSet)
 router.register(r'institutions', views.InstitutionViewSet)
 router.register(r'symbolicmusicfiles', views.SymbolicMusicFileViewSet)
+router.register(r'extractedfeatures', views.ExtractedFeaturesViewSet)
+router.register(r'encoders', views.EncoderViewSet)
 
 urlpatterns = [
     url(r'^$', front_end_views.HomeView.as_view(), name='home'),
