4597d25e36c3d4433d43ae4c9ab75f82b2cc31b9
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-26 10:23:11 -0400

    New: detail method to Encoder
    Refs #114

--- database/models/encoder.py
+++ database/models/encoder.py
@@ -8,15 +8,36 @@ class Encoder(EncoderValidatorBaseModel):
 
     def __str__(self):
         if self.user_id is not None:
-            return "{0} as encoder".format(self.user)
+            return "{0} (Encoder)".format(self.user)
         if self.software_id is not None:
-            return "{0} as encoder".format(self.software)
+            return "{0}".format(self.software)
         raise AssertionError('Neither User or Software is set')
 
     def prepare_summary(self):
         summary = {'display': self.__str__(),
                    'url': self.get_absolute_url()}
         return summary
+
+    def get_related(self):
+        related = {
+            'sym_files': {'list': self.symbolicmusicfile_set.all(),
+                          'model_name': 'Symbolic Music Files Encoded',
+                          'model_count': self.symbolicmusicfile_set.count()
+                          }
+        }
+
+        return related
+
+    def detail(self):
+        detail_dict = {
+            'title': self.__str__(),
+            'workflow': self.work_flow_text,
+            'notes': self.notes,
+            'related': self.get_related(),
+        }
+
+        return detail_dict
+
 
     class Meta(EncoderValidatorBaseModel.Meta):
         db_table = 'encoder'
