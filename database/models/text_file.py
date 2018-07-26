8bf4154245f6193fb329e6c27880fe7f7c3d9ebd
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-26 11:49:24 -0400

    New: detail method in TextFile
    Refs #114

--- database/models/text_file.py
+++ database/models/text_file.py
@@ -39,5 +39,22 @@ class TextFile(File):
                    }
         return summary
 
+    def detail(self):
+        detail_dict = {
+            'title':          self.__str__(),
+            'file_type':      self.file_type,
+            'version':        self.version,
+            'file_size':      self.file_size,
+            'encoding_date':  self.encoding_date,
+            'encoded_with':   self.encoded_with,
+            'validated_by':   self.validated_by,
+            'extra_metadata': self.extra_metadata,
+            'source':         self.manifests,
+            'file':           self.file,
+            'languages':      self.languages
+        }
+
+        return detail_dict
+
     class Meta(File.Meta):
         db_table = 'text_file'
