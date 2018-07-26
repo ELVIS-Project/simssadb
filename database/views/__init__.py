0acb27de8cfc9328ff178054be0c419ab8331323
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-26 13:38:58 -0400

    New: removed old PersonSearchView

--- database/views/__init__.py
+++ database/views/__init__.py
@@ -5,7 +5,7 @@ from database.views.institution import InstitutionViewSet
 from database.views.instrument import InstrumentViewSet
 from database.views.musical_work import MusicalWorkViewSet
 from database.views.part import PartViewSet
-from database.views.person import PersonSearchView, PersonViewSet
+from database.views.person import PersonViewSet
 from database.views.search import GeneralSearch
 from database.views.section import SectionViewSet
 from database.views.source import SourceViewSet
--- database/views/person.py
+++ database/views/person.py
@@ -1,8 +1,6 @@
 from database.views.generic_model_viewset import GenericModelViewSet
 from database.serializers import PersonSerializer
 from database.models.person import Person
-from drf_haystack.viewsets import HaystackViewSet
-from database.serializers import PersonSearchSerializer
 
 
 class PersonViewSet(GenericModelViewSet):
@@ -10,7 +8,3 @@ class PersonViewSet(GenericModelViewSet):
                                                      'sections_contributed_to').order_by('surname', 'given_name')
     serializer_class = PersonSerializer
 
-
-class PersonSearchView(HaystackViewSet):
-    index_models = [Person]
-    serializer_class = PersonSearchSerializer
