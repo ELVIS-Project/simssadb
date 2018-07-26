33df4c9f563b5861353c546dea8926a487612472
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-26 13:15:35 -0400

    New: removed old search serializers from __init__.py

--- database/serializers/__init__.py
+++ database/serializers/__init__.py
@@ -1,17 +1,11 @@
-from database.serializers.musical_work import MusicalWorkSerializer, \
-    MusicalWorkSearchSerializer
-from database.serializers.section import SectionSerializer, \
-    SectionSearchSerializer
-from database.serializers.part import PartSerializer, PartSearchSerializer
-from database.serializers.genre import GenreSerializer, GenreSearchSerializer
-from database.serializers.geographic_area import GeographicAreaSerializer, \
-    GeographicAreaSearchSerializer
-from database.serializers.instrument import InstrumentSerializer, \
-    InstrumentSearchSerializer
-from database.serializers.source import SourceSerializer, SourceSearchSerializer
-from database.serializers.person import PersonSerializer, PersonSearchSerializer
-from database.serializers.institution import InstitutionSerializer, \
-    InstitutionSearchSerializer
-from database.serializers.collection_of_sources import \
-    CollectionOfSourcesSerializer, CollectionOfSourcesSearchSerializer
+from database.serializers.musical_work import MusicalWorkSerializer
+from database.serializers.section import SectionSerializer
+from database.serializers.part import PartSerializer
+from database.serializers.genre import GenreSerializer
+from database.serializers.geographic_area import GeographicAreaSerializer
+from database.serializers.instrument import InstrumentSerializer
+from database.serializers.source import SourceSerializer
+from database.serializers.person import PersonSerializer
+from database.serializers.institution import InstitutionSerializer
+from database.serializers.collection_of_sources import CollectionOfSourcesSerializer
 from database.serializers.symbolic_music_file import SymbolicMusicFileSerializer
