3597d0566a408cc624a2b25dbece05733523db1c
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-07-26 11:44:30 -0400

    New: changed creators and curators fields to CharFields

--- database/models/research_corpus.py
+++ database/models/research_corpus.py
@@ -12,13 +12,12 @@ class ResearchCorpus(CustomBaseModel):
     features = models.ManyToManyField(ExtractedFeature,
                                       help_text='The features that this '
                                                 'Research Corpus contains')
-    # TODO: Change the creators and curators fields to be strings (not Users)
-    creators = models.ManyToManyField(User, related_name='created_corpora',
-                                      help_text='The creators of this '
-                                                'Research Corpus')
-    curators = models.ManyToManyField(User, related_name='curated_corpora',
-                                      help_text='The curators of this '
-                                                'Research Corpus')
+    creators = models.CharField(max_length=200,
+                                help_text='The creators of this '
+                                          'Research Corpus')
+    curators = models.CharField(max_length=200,
+                                help_text='The curators of this '
+                                          'Research Corpus')
     files = models.ManyToManyField(SymbolicMusicFile,
                                    help_text='The Symbolic Music Files that '
                                              'this Research Corpus contains')
