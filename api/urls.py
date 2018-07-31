74d3160ea9d54eee00a1644bb600f28f3c0b4af1
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2018-07-31 19:00:39 -0400

    WIP #44, now the message containing the metadata can display in the html

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from api.views import ViafComposerSearch, WikidataComposerSearch
+from api.views import ViafComposerSearch, WikidataComposerSearch, ViafComposerSearchAutoFill
 
 urlpatterns = [
     url(r'^search/wikidata/$', WikidataComposerSearch, name='wikidata-search'),
     url(r'^search/viaf/$', ViafComposerSearch, name='viaf-search'),
+    url(r'^search/viaf/auto-fill/$', ViafComposerSearchAutoFill, name='viaf-search-auto-fill'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,30 +1,63 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from api.viaf import ViafAPI
 from SPARQLWrapper import SPARQLWrapper, JSON
+from django.shortcuts import redirect
+from django.contrib import messages
 
-def ViafComposerSearch(request):
 
+def GetVIAFResult(request):
+    """
+    A function that gets VIAF result from the input text
+    :param request:
+    :return:
+    """
     if request.method == "GET" and 'q' in request.GET:
         value = request.GET['q']
         """Return JSON with suggested VIAF ids and display names."""
         viaf = ViafAPI()
 
         result = viaf.suggest(value)
+    return result, viaf
 
+
+def ViafComposerSearch(request):
+    result, viaf = GetVIAFResult(request)
         # check for empty search result and return empty json response
-        if result is None:
-            return JsonResponse({'results': []})
+    if result is None:
+        return JsonResponse({'results': []})
 
-        result = [item for item in result
-           if item['nametype'] == 'personal']
+    result = [item for item in result
+       if item['nametype'] == 'personal']
 
-        return JsonResponse({
-            'results': [dict(
-                uri=viaf.uri_from_id(item['viafid']),
-                id=item['viafid'],
-                text=item['displayForm'],
-            ) for item in result]
-        })
+    return JsonResponse({
+        'results': [dict(
+            uri=viaf.uri_from_id(item['viafid']),
+            id=item['viafid'],
+            text=item['displayForm'],
+        ) for item in result]
+    })
+
+
+def ViafComposerSearchAutoFill(request):
+    """
+    A function that parses the result of
+    :param request:
+    :return:
+    """
+    result = GetVIAFResult(request)
+    result_string = result[0][0]['displayForm']
+    metadata = [result_string.strip() for result_string in result_string.split(',')]
+    date = metadata[-1].split('-')
+    # the 2 lines of code below will refresh messages
+    storage = messages.get_messages(request)
+    storage.used = True
+    # Pass the context info into messages
+    messages.error(request, metadata[0], extra_tags='surname')
+    messages.error(request, metadata[1], extra_tags='given_name')
+    messages.error(request, date[0], extra_tags='birth_date')
+    messages.error(request, date[1], extra_tags='death_date')
+    return redirect('person')
+
 
 def WikidataComposerSearch(request):
 
