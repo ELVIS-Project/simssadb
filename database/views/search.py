a9909774b7f087d21fe3a05951a631129c2b8f69
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-08-07 15:49:48 -0400

    New: Search view now returns SymbolicMusicFiles instead of MusicalWorks
    Refs #136

--- database/views/search.py
+++ database/views/search.py
@@ -8,9 +8,8 @@ PAGE_SIZE = 25
 class TestFacet(FacetedSearchView):
     form_class = NiceFacetForm
     context_object_name = 'object_list'
-    facet_fields = ['places', 'dates', 'sym_formats', 'audio_formats',
-                    'text_formats', 'image_formats', 'certainty',
-                    'languages', 'religiosity', 'instruments',
-                    'composers', 'types', 'styles']
+    facet_fields = ['religiosity', 'instruments',
+                    'composers', 'types', 'styles',
+                    'certainty']
     template_name = 'search/test.html'
     paginate_by = PAGE_SIZE
