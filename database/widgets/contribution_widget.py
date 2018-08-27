425def4cbce34ba97ca5611193c32291ee699415
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-08-27 16:15:39 -0400

    New: MultiFieldWidget

---
+++ database/widgets/contribution_widget.py
@@ -0,0 +1,16 @@
+import json
+
+from django.forms import MultiWidget
+
+
+class MultiFieldWidget(MultiWidget):
+
+    def __init__(self, field_widgets, attrs=None):
+        super().__init__(field_widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            dict_value = json.loads(value)
+            print(dict_value)
+            return dict_value
+        return ""
