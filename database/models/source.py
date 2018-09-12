f00a73d3e23d103b347eda52d31a12f8560f4c15
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-09-12 14:21:13 -0400

    New: change Source to reflect data model changes
    Now it looks like the SourceInstantiation entity.
    It relates to one MusicalWork or multiple Sections or multiple Parts
    (exclusive or).
    It has a clean method to validate the above constraint.
    
    Constraints cannot be validated at the database level because it has
    many-to-many relationships.
    
    Refs #164

--- database/models/source.py
+++ database/models/source.py
@@ -1,10 +1,7 @@
-from django.contrib.postgres.fields import ArrayField
+from django.core.exceptions import ValidationError
 from django.db import models
 
-from database.models.collection_of_sources import CollectionOfSources
 from database.models.custom_base_model import CustomBaseModel
-from database.models.part import Part
-from database.models.section import Section
 
 
 class Source(CustomBaseModel):
@@ -13,79 +10,69 @@ class Source(CustomBaseModel):
 
     Must be part of a Collection of Sources.
     """
-    languages = ArrayField(models.CharField(max_length=200, blank=True),
-                           blank=True, null=True,
-                           help_text='The languages this Source is written in')
-    work = models.ForeignKey('MusicalWork', null=False, blank=False,
+    work = models.ForeignKey('MusicalWork',
+                             null=True,
+                             blank=True,
                              on_delete=models.PROTECT,
+                             related_name='source_instantiations',
                              help_text='The Musical Work manifested in part '
-                                       'or in full by this Source',
-                             default=0,
-                             related_name='sources')
-    sections = models.ManyToManyField(Section,
+                                       'or in full by this Source')
+    sections = models.ManyToManyField('Section',
+                                      blank=True,
+                                      related_name='source_instantiations',
                                       help_text='The Section or Sections '
                                                 'manifested in full by this '
-                                                'Source',
-                                      related_name='sources')
-    parts = models.ManyToManyField(Part,
+                                                'Source')
+    parts = models.ManyToManyField('Part',
+                                   blank=True,
+                                   related_name='source_instantiations',
                                    help_text='The Part or Parts '
                                              'manifested in full by this '
-                                             'Source',
-                                   related_name='sources')
-    part_of_collection = models.ForeignKey(CollectionOfSources,
-                                           related_name='sources',
-                                           null=False,
-                                           blank=False,
-                                           on_delete=models.PROTECT,
-                                           help_text='The Collection of '
-                                                     'Sources this Source '
-                                                     'belongs to')
-    parent_sources = models.ManyToManyField('self',
-                                            related_name='child_sources',
-                                            blank=True,
-                                            help_text='The Source this Source '
-                                                      'derives from',
-                                            symmetrical=False)
-    portion = models.TextField(max_length=255, blank=True, null=True,
-                               help_text='Specifies which portion of the '
-                                         'Collection of Sources this Source '
-                                         'represents. i.e. a page range, '
-                                         'a number of folios, a number of '
-                                         'files in a database, etc')
-    url = models.URLField(null=True, blank=True,
-                          help_text='An link back to an electronic version of '
-                                    'this Source, if it exists')
-
-    def __str__(self):
-        return "{0}, {1}".format(self.portion, self.part_of_collection.title)
-
-    @property
-    def encoders(self):
-        """Gets all the Encoders of files that manifest this Source"""
-        encoders = set()
-        for sym_file in self.manifested_by_sym_files:
-            encoders.add(sym_file.encoded_with)
-        for text_file in self.manifested_by_text_files:
-            encoders.add(text_file.encoded_with)
-        for audio_file in self.manifested_by_audio_files:
-            encoders.add(audio_file.encoded_with)
-        for image_file in self.manifested_by_image_files:
-            encoders.add(image_file.encoded_with)
-        return encoders
-
-    @property
-    def validators(self):
-        """Gets all the Validators of files that manifest this Source"""
-        validators = set()
-        for sym_file in self.manifested_by_sym_files:
-            validators.add(sym_file.encoded_with)
-        for text_file in self.manifested_by_text_files:
-            validators.add(text_file.encoded_with)
-        for audio_file in self.manifested_by_audio_files:
-            validators.add(audio_file.encoded_with)
-        for image_file in self.manifested_by_image_files:
-            validators.add(image_file.encoded_with)
-        return validators
+                                             'Source')
 
     class Meta(CustomBaseModel.Meta):
         db_table = 'source'
+
+    def clean(self):
+        """ Enforce the integrity of the relationship.
+
+        Ensure that at least one and only one of Musical Work/Section/Part
+        is not null.
+
+        Raises
+        ------
+        ValidationError
+            If more than one out Musical Work, Section or Part are not null
+            or if all three are null
+        """
+        if self.work is not None:
+            if self.sections.exists() or \
+                    self.parts.exists():
+                raise ValidationError('Only one of Work, Sections or '
+                                      'Part must be not null')
+        if self.sections.exists():
+            if self.parts.exists() or \
+                    self.work is not None:
+                raise ValidationError('Only one of Work, Sections or '
+                                      'Parts must be not null')
+        if self.work is not None:
+            if self.parts.exists() or \
+                    self.sections.exists():
+                raise ValidationError('Only one of Work, Sections or '
+                                      'Parts must be not null')
+        if not self.sections.exists() and \
+                not self.parts.exists() and \
+                self.work is None:
+            raise ValidationError('At least one of Work, Section or Part '
+                                  'must be not null')
+        super(CustomBaseModel, self).clean()
+
+    def save(self, *args, **kwargs):
+        """Save the current instance.
+
+        Overrides the parent method to ensure that clean() is called before
+        actually saving.
+
+        """
+        self.full_clean()
+        super(CustomBaseModel, self).save()
