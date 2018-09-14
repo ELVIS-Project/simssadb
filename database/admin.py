7ac26e7adba4af188b8917be252fd2a0d79b6043
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-09-14 14:40:23 -0400

    New: remove SourceInstantiation from admin.py

--- database/admin.py
+++ database/admin.py
@@ -12,7 +12,6 @@ from database.models.image_file import ImageFile
 from database.models.research_corpus import ResearchCorpus
 from database.models.experimental_study import ExperimentalStudy
 from database.models.extracted_feature import ExtractedFeature
-from database.models.source_instantiation import SourceInstantiation
 from database.models.source import Source
 from database.models.person import Person
 from database.models.institution import Institution
@@ -39,7 +38,6 @@ admin.site.register(ImageFile)
 admin.site.register(ResearchCorpus)
 admin.site.register(ExperimentalStudy)
 admin.site.register(ExtractedFeature)
-admin.site.register(SourceInstantiation)
 admin.site.register(Source)
 admin.site.register(Person)
 admin.site.register(Institution)
