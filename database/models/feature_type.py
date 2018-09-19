ba5b6e54d2eb1c111bf0cea5f416de323a34df66
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-09-19 14:24:41 -0400

    New: improve documentation for FeatureType
    Refs: #125

--- database/models/feature_type.py
+++ database/models/feature_type.py
@@ -1,3 +1,4 @@
+"""Define a FeatureType model"""
 from django.db import models
 from django.db.models import Max, Min
 
@@ -5,7 +6,42 @@ from database.models.custom_base_model import CustomBaseModel
 
 
 class FeatureType(CustomBaseModel):
-    name = models.CharField(max_length=200, blank=False, null=False,
+    """A category of Feature of which ExtractedFeatures are instances.
+
+    Attributes
+    ----------
+    FeatureType.name : models.CharField
+        The name of the FeatureType
+
+    FeatureType.code : models.CharField
+        The jSymbolic code of the FeatureType
+
+    FeatureType.description : models.TextField
+        A description of the FeatureType
+
+    FeatureType.is_sequential : models.NullBooleanField
+        Whether a feature can be extracted from sequential windows of a data
+        instance (e.g. individual measures, sections, etc); a value of true
+        means that it can, a value of false means that only one feature value
+        may be extracted per instance (i.e. per symbolic feature file)
+
+    FeatureType.dimensions : models.PositiveIntegerField
+        The number of dimensions of the FeatureType
+
+    FeatureType.min_val : models.FloatField
+        The minimum value of this FeatureType across all files that have this
+        feature
+
+    FeatureType.max_val : models.FloatField
+        The maximum value of this FeatureType across all files that have this
+        feature
+
+    FeatureType.instances : models.ManyToOneRel
+        The ExtractedFeature objects that are instances of this FeatureType
+    """
+    name = models.CharField(max_length=200,
+                            blank=False,
+                            null=False,
                             help_text='The name of the Extracted FeatureType')
     code = models.CharField(max_length=5, blank=False, null=False,
                             help_text='The jSymbolic code of the Extracted '
@@ -41,7 +77,8 @@ class FeatureType(CustomBaseModel):
                                           'Extracted FeatureType across all '
                                           'files that have this feature')
 
-    def max_and_min(self):
+    def max_and_min(self) -> None:
+        """Update the max and min values of this FeatureType"""
         if self.dimensions == 1:
             self.max_val = self.instances.all().aggregate(max_val=Max('value'))[
                 'max_val'][0]
@@ -50,7 +87,8 @@ class FeatureType(CustomBaseModel):
             self.save()
 
     @property
-    def group(self):
+    def group(self) -> str:
+        """Get the human readable group from the code of this FeatureType"""
         group = self.code.split('-')[0]
         if group == 'C':
             return 'Chords and Vertical Interval Features'
