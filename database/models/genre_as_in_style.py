ea0828e9bd7de05690a494745bc32fd85e9deaa9
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-09-19 14:58:22 -0400

    New: improve documentation for GenreAsInStyle

--- database/models/genre_as_in_style.py
+++ database/models/genre_as_in_style.py
@@ -5,16 +5,13 @@ from database.models.custom_base_model import CustomBaseModel
 
 class GenreAsInStyle(CustomBaseModel):
     """
-    Represents a musical genre (style)
+    Represents a musical genre as in style (Classical, Blues, Pop)
     """
     name = models.CharField(max_length=200, blank=False,
                             help_text='The name of the GenreAsInStyle')
 
-    def __str__(self):
-        return "{0}".format(self.name)
-
-    def count(self):
-        return self.style.count()
-
     class Meta(CustomBaseModel.Meta):
         db_table = 'genre_as_in_style'
+
+    def __str__(self):
+        return "{0}".format(self.name)
