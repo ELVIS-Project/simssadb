6bf4f1ad9976b079ef2cd3e91082f33eaf6ff56c
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-09-19 14:56:20 -0400

    New: move properties from SymbolicMusicFile to File
    These properties are common to all file types

--- database/models/file.py
+++ database/models/file.py
@@ -39,3 +39,136 @@ class File(CustomBaseModel):
 
     class Meta(CustomBaseModel.Meta):
         abstract = True
+
+    @property
+    def musical_work(self):
+        """Return the MusicalWork the Source of this File is
+        related to
+
+        Returns
+        -------
+        MusicalWork
+            The MusicalWork the Source of this File is related to
+        """
+        if self.manifests.work:
+            return self.manifests.work
+        elif self.manifests.sections.first():
+            return self.manifests.sections.first().musical_work
+        else:
+            return self.manifests.parts.first().section.musical_work
+
+    @property
+    def sections(self) -> QuerySet:
+        """Return the Sections manifested in full by the Source
+        of this File
+
+        Returns
+        -------
+        QuerySet
+            A QuerySet of all the Sections the Source of this
+            File is related to
+        """
+        return self.manifests.sections.all()
+
+    @property
+    def parts(self) -> QuerySet:
+        """Return the Parts manifested in full by the Source of
+        this File
+
+        Returns
+        -------
+        QuerySet
+            A QuerySet of all the Parts the Source of this File
+            is related to
+        """
+        return self.manifests.parts.all()
+
+    @property
+    def composers(self) -> QuerySet:
+        """Return the composers of the MusicalWork related to this File
+
+        Returns
+        -------
+        list
+            A list of strings representing the names of the composers
+        """
+        return self.musical_work.composers
+
+    @property
+    def sacred_or_secular(self) -> str:
+        """Return the _sacred_or_secular of the MusicalWork related to this file
+
+        Returns
+        -------
+        bool
+            The _sacred_or_secular of the MusicalWork related to this file
+        """
+        return self.musical_work.sacred_or_secular
+
+    @property
+    def certainty(self) -> bool:
+        """Return the certainty of the MusicalWork related to this File
+
+        Returns
+        -------
+        bool
+            The certainty of attribution of the MusicalWork related to this File
+        """
+        return self.musical_work.certainty_of_attributions
+
+    @property
+    def genres_as_in_type(self) -> QuerySet:
+        """Return the Genres (type) of the MusicalWork related to this File
+
+        Returns
+        -------
+        QuerySet
+            The Genres (type) of the MusicalWork related to this File
+        """
+        return self.musical_work.genres_as_in_type.all()
+
+    @property
+    def genres_as_in_style(self) -> QuerySet:
+        """Return the Genres (style) of the MusicalWork related to this File
+
+        Returns
+        -------
+        QuerySet
+            The Genres (style) of the MusicalWork related to this File
+        """
+        return self.musical_work.genres_as_in_style.all()
+
+    @property
+    def composers_dates(self) -> List[str]:
+        """Return the dates of the MusicalWork related to this File
+
+        Returns
+        -------
+        list
+            A list of date tuples representing the date ranges of composition
+        """
+        return self.musical_work.composers_dates
+
+    @property
+    def composers_locations(self) -> QuerySet:
+        """Return the places of the MusicalWork related to this File
+
+        Returns
+        -------
+        QuerySet
+            A QuerySet of GeographicAreas where the Musical Work related to
+            this File was composed
+        """
+        return self.musical_work.composers_locations
+
+    @property
+    def instrumentation(self) -> QuerySet:
+        """Return the Instruments of the MusicalWork related to this File
+
+        Returns
+        -------
+        QuerySet
+            A QuerySet of all the Instruments of the MusicalWork related to this
+            File
+        """
+        return self.musical_work.instrumentation
