1fba963cafd4757325ae7c4c614b357406132cfd
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-09-28 15:07:36 -0400

    New: improve documentation

--- database/models/genre_as_in_style.py
+++ database/models/genre_as_in_style.py
@@ -7,8 +7,22 @@ from database.models.custom_base_model import CustomBaseModel
 class GenreAsInStyle(CustomBaseModel):
     """
     Represents a musical genre as in style (Classical, Blues, Pop)
+
+    Attributes
+    ----------
+    GenreAsInStyle.name : models.CharField
+        The name of this style
+
+    GenreAsInStyle.musical_works : models.ManyToManyRel
+        References to the MusicalWorks of this style
+
+    See Also
+    --------
+    database.models.CustomBaseModel
+    database.models.MusicalWork
     """
-    name = models.CharField(max_length=200, blank=False,
+    name = models.CharField(max_length=200,
+                            blank=False,
                             help_text='The name of the GenreAsInStyle')
 
     class Meta(CustomBaseModel.Meta):
