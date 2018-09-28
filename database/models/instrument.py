4f5f663f2869c6e8b0213fb8016ac744f3efac19
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-09-28 15:09:36 -0400

    New: optimize imports

--- database/models/instrument.py
+++ database/models/instrument.py
@@ -1,5 +1,7 @@
 """Define a Instrument model"""
+from django.apps import apps
 from django.db import models
+from django.db.models import QuerySet
 
 from database.models.custom_base_model import CustomBaseModel
 
@@ -10,19 +12,41 @@ class Instrument(CustomBaseModel):
 
     A part is written for an instrument or voice, and a symbolic music file
     can specify which instrument or voices it contains
+
+    Attributes
+    ----------
+    Instrument.name : models.CharField
+        The name of this Instrument
+
+    Instrument.parts : models.ManyToOneRel
+        References to Parts that use this Instrument
+
+    Instrument.sym_files : models.ManyToManyRel
+        References to SymbolicMusicFiles that specify this Instrument
     """
     name = models.CharField(max_length=200,
                             help_text='The name of the Instrument or Voice')
 
+    class Meta:
+        db_table = 'instrument'
+
     def __str__(self):
         return "{0}".format(self.name)
 
-    def sections(self):
-        """Returns all the sections that use this instrument"""
-        sections = set()
-        for part in self.part_written_for.all():
-            sections.add(part.in_section)
+    @property
+    def sections(self) -> QuerySet:
+        """Get all the Sections that use this Instrument."""
+        section_model = apps.get_model('database', 'section')
+        sections = section_model.objects.none()
+        for part in self.parts.all():
+            sections.union(part.in_section)
         return sections
 
-    class Meta:
-        db_table = 'instrument'
+    @property
+    def musical_works(self) -> QuerySet:
+        """Get all the MusicalWorks that use this Instrument."""
+        work_model = apps.get_model('database', 'musical_work')
+        works = work_model.objects.none()
+        for section in self.sections:
+            works.union(section.musical_work)
+        return works
--- database/models/source.py
+++ database/models/source.py
@@ -1,49 +1,120 @@
+"""Define a Source model"""
 from django.core.exceptions import ValidationError
 from django.db import models
 
-from database.models.custom_base_model import CustomBaseModel
+from database.models import CustomBaseModel
 
 
 class Source(CustomBaseModel):
-    """
-    Represents a document containing the music defining a Work/Section/Part
+    """A document containing the music defining a MusicalWork or a
+    set of Sections or a set of Parts.
 
-    Must be part of a Collection of Sources.
+    Must be part of a CollectionOfSources.
+    If a CollectionOfSources contains only one Source, the Source is trivial
+    but still must exist, i.e., an CollectionOfSources without at least one
+    Source cannot exist.
+
+    A Source must refer to either one MusicalWork, or a set of Sections or a
+    set of Parts. For example, it cannot refer to both a MusicalWork and a
+    set of Sections or a set of Sections and a set of Parts.
+
+    A Source can be derived from a parent Source, implying a chain of
+    provenance.
+
+    A Source is manifested by digital files of many types such as audio,
+    text, symbolic or image.
+
+    Attributes
+    ----------
+    Source.portion : models.CharField
+        A description of which portion of the CollectionOfSources this Source
+        represents, for instance, page numbers or folio
+
+    Source.collection : models.ForeignKey
+        Reference to the CollectionOfSources this Source belongs to
+
+    Source.parent_source : models.ForeignKey
+        Reference to the Source this Source was derived from
+
+    Source.child_sources : models.ManyToOneRel
+        References to Sources derived from this Source
+
+    Source.work : models.ForeignKey
+        Reference to a MusicalWork that is defined in full by this Source
+        
+    Source.sections : models.ManyToManyField
+        References to Sections that are defined in full by this Source
+        
+    Source.parts : models.ManyToManyField
+        References to Parts that are defined in full by this Source
+        
+    Source.manifested_by_audio_files : models.ManyToOneRel
+        References to AudioFiles that manifest this Source
+    
+    Source.manifested_by_text_files : models.ManyToOneRel
+        References to TextFiles that manifest this Source
+    
+    Source.manifested_by_image_files : models.ManyToOneRel
+        References to ImageFiles that manifest this Source
+    
+    Source.manifested_by_sym_files : models.ManyToOneRel
+        References to SymbolicMusicFiles that manifest this Source
     """
+    collection = models.ForeignKey('CollectionOfSources',
+                                   null=False,
+                                   blank=False,
+                                   on_delete=models.PROTECT,
+                                   related_name='sources')
+
+    portion = models.CharField(max_length=200,
+                               null=False,
+                               blank=False,
+                               help_text=' A description of which portion of '
+                                         'the CollectionOfSources this Source '
+                                         'represents, for instance, '
+                                         'page numbers or folio')
+    parent_source = models.ForeignKey('self',
+                                      null=True,
+                                      blank=True,
+                                      on_delete=models.PROTECT,
+                                      related_name='child_sources')
     work = models.ForeignKey('MusicalWork',
                              null=True,
                              blank=True,
                              on_delete=models.PROTECT,
-                             related_name='source_instantiations',
+                             related_name='sources',
                              help_text='The Musical Work manifested in part '
                                        'or in full by this Source')
     sections = models.ManyToManyField('Section',
                                       blank=True,
-                                      related_name='source_instantiations',
+                                      related_name='sources',
                                       help_text='The Section or Sections '
                                                 'manifested in full by this '
                                                 'Source')
     parts = models.ManyToManyField('Part',
                                    blank=True,
-                                   related_name='source_instantiations',
+                                   related_name='sources',
                                    help_text='The Part or Parts '
                                              'manifested in full by this '
                                              'Source')
 
     class Meta(CustomBaseModel.Meta):
-        db_table = 'source'
+        db_table = 'source_instantiation'
 
-    def clean(self):
+    def __str__(self):
+        return ""
+
+    def clean(self) -> None:
         """ Enforce the integrity of the relationship.
 
-        Ensure that at least one and only one of Musical Work/Section/Part
+        Ensure that at least one and only one of MusicalWork/Sections/Parts
         is not null.
 
         Raises
         ------
         ValidationError
-            If more than one out Musical Work, Section or Part are not null
-            or if all three are null
+            If more than one out MusicalWork, Sections or Parts are not null
+            or if all three are null.
         """
         if self.work is not None:
             if self.sections.exists() or \
@@ -66,13 +137,3 @@ class Source(CustomBaseModel):
             raise ValidationError('At least one of Work, Section or Part '
                                   'must be not null')
         super(CustomBaseModel, self).clean()
-
-    def save(self, *args, **kwargs):
-        """Save the current instance.
-
-        Overrides the parent method to ensure that clean() is called before
-        actually saving.
-
-        """
-        self.full_clean()
-        super(CustomBaseModel, self).save()
