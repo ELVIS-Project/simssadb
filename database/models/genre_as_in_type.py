0abafc468b5d43422839d361ef012b2c3c3d9f2a
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-09-28 15:07:42 -0400

    New: improve documentation

--- database/models/genre_as_in_type.py
+++ database/models/genre_as_in_type.py
@@ -7,8 +7,22 @@ from database.models.custom_base_model import CustomBaseModel
 class GenreAsInType(CustomBaseModel):
     """
     Represents a musical genre as in type of work (Motet, Symphony, Mass)
+
+    Attributes
+    ----------
+    GenreAsInType.name : models.CharField
+        The name of this type of work
+
+    GenreAsInType.musical_works : models.ManyToManyRel
+        References to the MusicalWorks of this type
+
+    See Also
+    --------
+    database.models.CustomBaseModel
+    database.models.MusicalWork
     """
-    name = models.CharField(max_length=200, blank=False,
+    name = models.CharField(max_length=200,
+                            blank=False,
                             help_text='The name of the GenreAsInStyle')
 
     class Meta(CustomBaseModel.Meta):
