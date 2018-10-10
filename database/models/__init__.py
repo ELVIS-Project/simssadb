8bf1558d0a3f4d51d348a00e0e92b8b7320806f3
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-10-10 15:10:07 -0400

    Update the __init__.py file in the Models module

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -11,6 +11,7 @@ The models are:
 * EncoderValidatorBaseModel - A base model for Encoder and Validator
 * ExperimentalStudy - A study based on Files from a particular Research Corpus
 * ExtractedFeature - Content-based data extracted from a file
+* FeatureType - A category of Feature of which ExtractedFeatures are instances
 * File - Base abstract model with fields common to all file types
 * GenreAsInStyle - A musical genre (type of work or style)
 * GeographicArea - A geographic area that can be part of another are
@@ -20,11 +21,11 @@ The models are:
 * MusicalWork - A complete work of music
 * Part - A single voice or instrument in a Section of a Musical Work
 * Person - A real world person that contributed to a musical work
-* Profile - Extends the user model to allow for extra data
 * ResearchCorpus - A collection of files that can be used in a ExperimentStudy
 * Section - A component of a Musical Work e.g. an Aria in an Opera
 * Software - A Software that encoded, validated or extracted features files
 * Source - A document containing the music of a Musical Work/Section/Part
+* SourceInstantiation - An abstract entity defined by the music in a Source
 * SymbolicMusicFile - A manifestation of a Source as a symbolic music file
 * TextFile - A manifestation of a Source as a digital Text file
 * Validator - A User or Software that verified the quality of files
@@ -39,6 +40,7 @@ from database.models.encoder_validator_base_model import \
     EncoderValidatorBaseModel
 from database.models.experimental_study import ExperimentalStudy
 from database.models.extracted_feature import ExtractedFeature
+from database.models.feature_type import FeatureType
 from database.models.file import File
 from database.models.genre_as_in_style import GenreAsInStyle
 from database.models.genre_as_in_type import GenreAsInType
@@ -53,7 +55,7 @@ from database.models.research_corpus import ResearchCorpus
 from database.models.section import Section
 from database.models.software import Software
 from database.models.source import Source
+from database.models.source_instantiation import SourceInstantiation
 from database.models.symbolic_music_file import SymbolicMusicFile
 from database.models.text_file import TextFile
 from database.models.validator import Validator
-from database.models.feature_type import FeatureType
