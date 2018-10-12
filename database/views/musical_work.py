17cd7420013cd45ef38bd6e7cf1a16de105018c4
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-10-12 15:53:41 -0400

    Declare fields for MusicalWorkViewSet

--- database/views/musical_work.py
+++ database/views/musical_work.py
@@ -8,3 +8,18 @@ class MusicalWorkViewSet(GenericModelViewSet):
             'sections__parts__written_for').order_by(
             'variant_titles')
     serializer_class = MusicalWorkSerializer
+    summary_fields = ['composers']
+    detail_fields = ['variant_titles',
+                     'genres_as_in_style',
+                     'genres_as_in_type',
+                     'certainty_of_attributions',
+                     'sacred_or_secular',
+                     'collections_of_sources',
+                     'instrumentation',
+                     'contributions',
+                     'languages']
+    related_fields = [{
+        'object_name': 'sections',
+        'fields':      ['parent_sections', 'child_sections'],
+        'badge':       'parts'
+        }]
