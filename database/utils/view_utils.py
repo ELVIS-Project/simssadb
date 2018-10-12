72aa754acf06ababed050d255bb6ec5f1329638a
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-10-12 15:56:38 -0400

    Quick bug fixes

--- database/utils/view_utils.py
+++ database/utils/view_utils.py
@@ -7,35 +7,27 @@ from django.db.models.query import QuerySet
 from database.models.custom_base_model import CustomBaseModel
 
 
-def make_related_objects_dict(relationship_name: str,
-                              related_object_fields: List[str],
-                              badge: Optional[str] = None) -> dict:
-    return {
-        'relationship_name': relationship_name,
-        'fields':            related_object_fields,
-        'badge':             badge
-        }
-
-
 def make_fields_dict(instance: CustomBaseModel,
                      fields_list: List[str]) -> dict:
     fields_dict = {}
+    if fields_list:
+        for field in fields_list:
+            key = field
 
-    for field in fields_list:
-        key = field
-        try:
-            value = getattr(instance, field)
-            if isinstance(value, Manager):
-                value = list(value.all())
-            elif isinstance(value, Iterable) and not isinstance(value, str):
-                value = list(value)
+            try:
+                value = getattr(instance, field)
+                if isinstance(value, Manager):
+                    value = list(value.all())
+                elif isinstance(value, Iterable) and not isinstance(value, str):
+                    value = list(value)
 
-            fields_dict.update({key: value})
-        except AttributeError:
-            warnings.simplefilter('always')
-            warning_string = 'Did not find the field ' \
-                             + key + ' specified in fields_list'
-            warnings.warn(warning_string)
+                fields_dict.update({key: value})
+
+            except AttributeError:
+                warnings.simplefilter('always')
+                warning_string = 'Did not find the field ' \
+                                 + key + ' specified in fields_list'
+                warnings.warn(warning_string)
 
     return fields_dict
 
@@ -51,13 +43,12 @@ def make_summary_dict(instance: CustomBaseModel,
         summary_dict['badge_name'] = badge_field
         badge_list = getattr(instance, badge_field)
         if isinstance(badge_list, Manager):
-            badge_list = list(badge_list.all())
-
-        if isinstance(badge_list, Iterable) and not isinstance(badge_list,
-                                                               str):
+            badge_count = badge_list.count()
+        elif isinstance(badge_list, Iterable) and not isinstance(badge_list,
+                                                                 str):
             badge_list = list(badge_list)
-
-        if isinstance(badge_list, Sized):
+            badge_count = len(badge_list)
+        elif isinstance(badge_list, Sized):
             badge_count = len(badge_list)
         else:
             badge_count = 1
@@ -69,55 +60,55 @@ def make_summary_dict(instance: CustomBaseModel,
 def make_related_dict(instance: CustomBaseModel,
                       related_objects: List[dict]) -> dict:
     related_dict = {}
+    if related_objects:
+        for related_object in related_objects:
+            key = related_object['object_name']
+            sub_fields = related_object['fields']
 
-    for related_object in related_objects:
-        key = related_object['object_name']
-        sub_fields = related_object['fields']
+            if 'badge' in related_object:
+                badge_field = related_object['badge']
+            else:
+                badge_field = None
 
-        if 'badge' in related_object:
-            badge_field = related_object['badge']
-        else:
-            badge_field = None
+            try:
+                value_list = getattr(instance, key)
 
-        try:
-            value_list = getattr(instance, key)
+                if isinstance(value_list, Manager):
+                    value_list = value_list.all()
 
-            if isinstance(value_list, Manager):
-                value_list = value_list.all()
+                if not isinstance(value_list, QuerySet):
+                    raise TypeError
 
-            if not isinstance(value_list, QuerySet):
-                raise TypeError
+                model_name = value_list.model.get_verbose_name_plural()
+                model_count = value_list.count()
+                value_list = list(value_list)
 
-            model_name = value_list.model.get_verbose_name_plural()
-            value_list = list(value_list)
-            model_count = len(value_list)
+                summary_list = []
+                for value in value_list:
+                    summary = make_summary_dict(value,
+                                                sub_fields,
+                                                badge_field)
+                    summary_list.append(summary)
 
-            summary_list = []
-            for value in value_list:
-                summary = make_summary_dict(value,
-                                            sub_fields,
-                                            badge_field)
-                summary_list.append(summary)
+                sub_dict = {
+                    'list':        summary_list,
+                    'model_name':  model_name,
+                    'model_count': model_count
+                    }
 
-            sub_dict = {
-                'list':        summary_list,
-                'model_name':  model_name,
-                'model_count': model_count
-                }
+                related_dict.update({key: sub_dict})
 
-            related_dict.update({key: sub_dict})
+            except AttributeError:
+                warnings.simplefilter('always')
+                warning_string = 'Did not find the field ' \
+                                 + key + ' specified in related_fields.'
+                warnings.warn(warning_string)
 
-        except AttributeError:
-            warnings.simplefilter('always')
-            warning_string = 'Did not find the field ' \
-                             + key + ' specified in related_fields.'
-            warnings.warn(warning_string)
-
-        except TypeError:
-            warnings.simplefilter('always')
-            warning_string = 'The field ' + key + ' does not refer to a ' \
-                                                  'QuerySet'
-            warnings.warn(warning_string)
+            except TypeError:
+                warnings.simplefilter('always')
+                warning_string = 'The field ' + key + ' does not refer to a ' \
+                                                      'QuerySet'
+                warnings.warn(warning_string)
 
     return related_dict
 
