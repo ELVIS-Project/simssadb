6c6bd80e0f021ea34b6ee83d9f37d0767b1c6606
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-10-17 13:52:15 -0400

    Declare fields for both genre viewsets

--- database/views/genre_as_in_style.py
+++ database/views/genre_as_in_style.py
@@ -1,6 +1,11 @@
 from database.models.genre_as_in_style import GenreAsInStyle
 from database.views.generic_model_viewset import GenericModelViewSet
+from database.views.generic_model_viewset import DetailedAttribute
 
 
 class GenreAsInStyleViewSet(GenericModelViewSet):
     queryset = GenreAsInStyle.objects.all()
+    badge_field = ['musical_works']
+    detailed_attributes = [DetailedAttribute(attribute_name='musical_works',
+                                             fields=['composers'],
+                                             badge_field='sections')]
--- database/views/genre_as_in_type.py
+++ database/views/genre_as_in_type.py
@@ -1,6 +1,11 @@
 from database.models.genre_as_in_type import GenreAsInType
+from database.views.generic_model_viewset import DetailedAttribute
 from database.views.generic_model_viewset import GenericModelViewSet
 
 
 class GenreAsInTypeViewSet(GenericModelViewSet):
     queryset = GenreAsInType.objects.all()
+    badge_field = ['musical_works']
+    detailed_attributes = [DetailedAttribute(attribute_name='musical_works',
+                                             fields=['composers'],
+                                             badge_field='sections')]
