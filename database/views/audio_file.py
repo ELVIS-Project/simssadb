b9a340f2be6d9fc2e914c16c11803f081cec30ee
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-10-17 13:21:23 -0400

    Declare fields for AudioFileViewSet

--- database/views/audio_file.py
+++ database/views/audio_file.py
@@ -6,3 +6,18 @@ from database.views.generic_model_viewset import GenericModelViewSet
 class AudioFileViewSet(GenericModelViewSet):
     queryset = AudioFile.objects.all()
     serializer_class = AudioFileSerializer
+    summary_fields = ['file_type', 'manifests']
+    detail_fields = ['encoded_with',
+                     'encoding_date',
+                     'file_size',
+                     'file_type',
+                     'extra_metadata',
+                     'length',
+                     'recording_date',
+                     'validated_by',
+                     'version',
+                     'musical_work',
+                     'sections',
+                     'parts',
+                     'manifests',
+                     'file']
