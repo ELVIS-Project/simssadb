a66ca5472701a2db1cb83c3c6106b338c6c0443a
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-10-17 13:30:54 -0400

    Declare fields for ContributionViewSet

--- database/views/contribution.py
+++ database/views/contribution.py
@@ -6,3 +6,11 @@ from database.views.generic_model_viewset import GenericModelViewSet
 class ContributionViewSet(GenericModelViewSet):
     queryset = Contribution.objects.all()
     serializer_class = ContributionSerializer
+    summary_fields = ['person', 'role', 'location', 'date', 'certain']
+    detailed_attributes = ['person',
+                           'role',
+                           'date',
+                           'location',
+                           'contributed_to_part',
+                           'contributed_to_section',
+                           'contributed_to_work']
