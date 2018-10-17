452dd848b7d27032f14ab5d70fe587fe8f1dd809
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-10-17 14:12:53 -0400

    Add badge field in ImageFileViewSet
    Refs #169

--- database/views/image_file.py
+++ database/views/image_file.py
@@ -6,3 +6,16 @@ from database.views.generic_model_viewset import GenericModelViewSet
 class ImageFileViewSet(GenericModelViewSet):
     queryset = ImageFile.objects.all()
     serializer_class = ImageFileSerializer
+    summary_fields = ['file_type', 'manifests']
+    detail_fields = ['encoded_with',
+                     'encoding_date',
+                     'file_size',
+                     'file_type',
+                     'extra_metadata',
+                     'validated_by',
+                     'version',
+                     'musical_work',
+                     'sections',
+                     'parts',
+                     'manifests',
+                     'file']
