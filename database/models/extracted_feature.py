dc230cbb7fcea5842caea3a5cfbaf9cf9c99ced0
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2018-10-26 17:38:04 -0400

    Change __str__ function of extracted_feature to handle histograms

--- database/models/extracted_feature.py
+++ database/models/extracted_feature.py
@@ -65,8 +65,10 @@ class ExtractedFeature(CustomBaseModel):
         db_table = 'extracted_feature'
 
     def __str__(self):
-        return "{0}: {1} ({2})".format(self.instance_of_feature.name,
-                                       self.value, self.feature_of)
+        if not self.is_histogram:
+            return "{0}: {1}".format(self.instance_of_feature.name, self.value[0])
+        else:
+            return self.name
 
     def clean(self) -> None:
         """Check if length of value is the same as the declared dimensions"""
