34f0a7df5a927c791fe568350e45ac316ab20c38
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2018-11-19 15:30:31 -0500

    Fix #175, pre-fill the DB with data from Wikidata

--- api/autofill.py
+++ api/autofill.py
@@ -15,7 +15,51 @@ def autofill_composer():
                                                    OPTIONAL { ?item wdt:P20 ?place_of_death. }
                                                    ?item wdt:P735 ?given_name. 
                                                    ?item wdt:P734 ?family_name.
-                                                   FILTER((YEAR(?date_of_birth)) < 1900)
+                                                   FILTER((YEAR(?date_of_birth)) <= 1850)
+                                                   }
+                                               """
+
+                                   )
+    return result
+
+
+def autofill_composer2():
+    result = WikidataQuery(
+                                   """
+                                                   SELECT ?item ?label ?date_of_birth ?date_of_death ?place_of_birth ?place_of_birthLabel ?place_of_death ?place_of_deathLabel ?given_name ?given_nameLabel ?family_name ?family_nameLabel WHERE {
+                                                   ?item wdt:P106 wd:Q36834.
+                                                   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+                                                   ?item rdfs:label ?label.
+                                                   FILTER((LANG(?label)) = "en")
+                                                   ?item wdt:P569 ?date_of_birth.
+                                                   ?item wdt:P570 ?date_of_death.
+                                                   OPTIONAL { ?item wdt:P19 ?place_of_birth. }
+                                                   OPTIONAL { ?item wdt:P20 ?place_of_death. }
+                                                   ?item wdt:P735 ?given_name. 
+                                                   ?item wdt:P734 ?family_name.
+                                                   FILTER((YEAR(?date_of_birth)) > 1850 && (YEAR(?date_of_birth)) <= 1920 )
+                                                   }
+                                               """
+
+                                   )
+    return result
+
+
+def autofill_composer3():
+    result = WikidataQuery(
+                                   """
+                                                   SELECT ?item ?label ?date_of_birth ?date_of_death ?place_of_birth ?place_of_birthLabel ?place_of_death ?place_of_deathLabel ?given_name ?given_nameLabel ?family_name ?family_nameLabel WHERE {
+                                                   ?item wdt:P106 wd:Q36834.
+                                                   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+                                                   ?item rdfs:label ?label.
+                                                   FILTER((LANG(?label)) = "en")
+                                                   ?item wdt:P569 ?date_of_birth.
+                                                   ?item wdt:P570 ?date_of_death.
+                                                   OPTIONAL { ?item wdt:P19 ?place_of_birth. }
+                                                   OPTIONAL { ?item wdt:P20 ?place_of_death. }
+                                                   ?item wdt:P735 ?given_name. 
+                                                   ?item wdt:P734 ?family_name.
+                                                   FILTER((YEAR(?date_of_birth)) > 1920 )
                                                    }
                                                """
 
