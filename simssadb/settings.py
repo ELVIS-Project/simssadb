83b9fca4eabb19c80ccb165826abc93222b9745f
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-02-14 14:19:57 -0500

    Add static root folder

--- simssadb/settings.py
+++ simssadb/settings.py
@@ -183,6 +183,7 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
 
 if DEBUG:
