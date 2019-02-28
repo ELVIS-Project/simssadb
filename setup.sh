5767cce523c2217d718c7478afb139574c9d32c7
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-02-27 21:53:35 -0500

    Move variables to setup.sh and get them with setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-# Django app (simmsadb) variables
-# The app is reading those with os.getenv()
-
-# Django settings
-export SIMSSADB_HOSTS=['*']
-export SIMSSADB_DEBUG=True
-export SIMSSADB_SECRET_KEY="f1(1=m5ze=@ne023nnabwz(%x^j+8!y+py&n#lwvo0&(#c"
-
-# postgres settings
-export POSTGRES_DB=simssadb
-export SIMSSADB_DB_HOST=localhost
-export SIMSSADB_DB_PORT=5432
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=debug
-
-# email settings
-export SIMSSADB_EMAIL_HOST=smtp.gmail.com
-export SIMSSADB_EMAIL_HOST_USER=@gmail.com
-export SIMSSADB_EMAIL_HOST_PASSWORD=PASSWORD
-export SIMSSADB_EMAIL_PORT=587
-
-# haystack settings
-export SIMSSADB_HAYSTACK_URL=http://elasticsearch:9200/
-
 # Install dependencies with homebrew
 brew install python3
 brew upgrade python3
@@ -35,11 +11,15 @@ brew install elasticsearch@5.6
 brew services start postgresql
 brew services start elasticsearch@5.6
 
+#Setup environment
+cat env.txt >> ~/.bash_profile
+source ~/.bash_profile
+
 # Set up database
-createdb $POSTGRES_DB
-createuser $POSTGRES_USER -d -e -l -r -s
-psql -d $POSTGRES_DB -c "ALTER DATABASE $POSTGRES_DB OWNER TO $POSTGRES_USER;"
-psql $POSTGRES_DB -c "ALTER USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
+createdb ${POSTGRES_DB}
+createuser ${POSTGRES_USER} -d -e -l -r -s
+psql -d ${POSTGRES_DB} -c "ALTER DATABASE $POSTGRES_DB OWNER TO $POSTGRES_USER;"
+psql ${POSTGRES_DB} -c "ALTER USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
 
 # Set up python virtual environment
 python3 -m venv venv
