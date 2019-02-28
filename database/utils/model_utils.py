002674bf404f73a1fcb21ad0573a3733e4e8b30b
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-02-28 02:56:26 -0500

    Manage handling dates better

--- database/utils/model_utils.py
+++ database/utils/model_utils.py
@@ -1,8 +1,10 @@
 """A collection of functions that are useful for models"""
+from typing import Union
+
 from psycopg2._range import DateRange
 
 
-def clean_date(date_range: DateRange) -> str:
+def clean_date(date_range: DateRange) -> Union[str, bool]:
     """Format a date_range object to a nice string
 
     Parameters
@@ -15,14 +17,18 @@ def clean_date(date_range: DateRange) -> str:
         A nicely formatted date, either YYYY or YYYY-YYYY
     """
     if date_range is not None:
-        if date_range.lower is not None and date_range.upper is not None:
-            if date_range.lower.year == date_range.upper.year:
-                date = str(date_range.upper.year)
+        try:
+            if date_range.lower is not None and date_range.upper is not None:
+                if date_range.lower == date_range.upper:
+                    date = str(date_range.upper)
+                else:
+                    date = '(' + str(date_range.lower) + '-' + str(
+                            date_range.upper) + ')'
+            elif date_range.lower is not None and date_range.upper is None:
+                date = str(date_range.lower)
             else:
-                date = '(' + str(date_range.lower.year) + '-' + str(
-                        date_range.upper.year) + ')'
-        elif date_range.lower is not None and date_range.upper is None:
-            date = str(date_range.lower.year)
-        else:
-            date = str(date_range.upper.year)
-        return date
+                date = str(date_range.upper)
+            return date
+        except ValueError:
+            return False
+    return False
