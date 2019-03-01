a89c68fcdf20047e47afeefe92cf31377a9578a4
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-03-01 12:44:54 -0500

    Add signal to run jSymbolic when file is saved

---
+++ database/signals.py
@@ -0,0 +1,13 @@
+from database.models.symbolic_music_file import SymbolicMusicFile
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import feature_extraction.single_file_feature_extraction
+import os
+
+
+@receiver(post_save, sender=SymbolicMusicFile)
+def run_jsymbolic(sender, instance, **kwargs):
+    path = os.path.join(os.getcwd() + '/media/', instance.file.name)
+    print(path)
+    print(os.path.exists(path))
+    feature_extraction.single_file_feature_extraction.driver(path)
