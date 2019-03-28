7c41306aa10147a382083e39e8e000a200465a2e
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-03-28 11:51:03 -0400

    Add early versions of post, form_valid, form_invalid

--- database/views/creation_view.py
+++ database/views/creation_view.py
@@ -34,8 +34,42 @@ class CreationView(FormView):
                                       contribution_form=contribution_form,
                                       source_form=source_form,
                                       parent_source_form=parent_source_form))
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests, instantiating a form instance and its inline
+        formsets with the passed POST variables and then checking them for
+        validity.
+        """
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        contribution_form = self.ContributionFormSet(self.request.POST)
+        source_form = CollectionOfSourcesForm(self.request.POST)
+        parent_source_form = CollectionOfSourcesForm(self.request.POST)
+        if (form.is_valid() and contribution_form.is_valid() and
+                source_form.is_valid() and parent_source_form.is_valid()):
+            return self.form_valid(form, contribution_form, source_form,
+                                   parent_source_form)
+        else:
+            return self.form_invalid(form, contribution_form, source_form,
+                                     parent_source_form)
+
+    def form_valid(self, form, contribution_form, source_form,
+                   parent_source_form):
+        """
+        Called if all forms are valid.
+        """
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form, contribution_form, source_form,
+                     parent_source_form):
+        """
+        Called if a form is invalid. Re-renders the context data with the
+        data-filled forms and errors.
+        """
         return self.render_to_response(
             self.get_context_data(form=form,
                                   contribution_form=contribution_form,
-                                  collection_form=collection_form,
-                                  file_form=file_form))
+                                  source_form=source_form,
+                                  parent_source_form=parent_source_form))
