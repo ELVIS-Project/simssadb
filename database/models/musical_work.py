872777288a22e14e9bf7bb48d6086334eec3d42b
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-04-02 13:41:37 -0400

    Handle printing sacred_or_secular properly

--- database/models/musical_work.py
+++ database/models/musical_work.py
@@ -136,9 +136,9 @@ class MusicalWork(FileAndSourceInfoMixin, ContributionInfoMixin,
     @property
     def sacred_or_secular(self) -> str:
         """Get the sacred_or_secular value as a human friendly string."""
-        if self._sacred_or_secular:
-            return 'Sacred'
-        if not self._sacred_or_secular:
-            return 'Secular'
         if self._sacred_or_secular is None:
             return 'Non Applicable'
+        if self._sacred_or_secular:
+            return 'Sacred'
+        if self._sacred_or_secular is False:
+            return 'Secular'
