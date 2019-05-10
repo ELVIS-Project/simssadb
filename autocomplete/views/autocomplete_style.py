e57102ab58da190a2af9aa9d38943be76ce7462a
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-05-10 10:18:09 -0400

    Create autocomplete view for Style
    Refs #258

---
+++ autocomplete/views/autocomplete_style.py
@@ -0,0 +1,15 @@
+from dal import autocomplete
+from autocomplete.models import AutocompleteStyle
+
+
+class AutocompleteStyle(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = AutocompleteStyle.objects.all()
+
+        if self.q:
+            qs = qs.filter(name__istartswith=self.q)
+
+        return qs
+
+    def has_add_permission(self, request):
+        return True
