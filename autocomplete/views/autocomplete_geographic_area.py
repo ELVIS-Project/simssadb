0ff47256c4a33ce2f29315adc5228d97b99a5054
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-05-10 10:15:04 -0400

    Create autocomplete view for GeographicArea
    Refs #258

---
+++ autocomplete/views/autocomplete_geographic_area.py
@@ -0,0 +1,15 @@
+from dal import autocomplete
+from autocomplete.models import AutocompleteGeographicArea
+
+
+class AutocompleteGeographicArea(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = AutocompleteGeographicArea.objects.all()
+
+        if self.q:
+            qs = qs.filter(name__istartswith=self.q)
+
+        return qs
+
+    def has_add_permission(self, request):
+        return True
