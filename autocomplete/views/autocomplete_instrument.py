5d0d7390199ddab85e4304a7f8f6b17627055733
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-05-10 10:16:05 -0400

    Create autocomplete view for Instrument
    Refs #258

---
+++ autocomplete/views/autocomplete_instrument.py
@@ -0,0 +1,15 @@
+from dal import autocomplete
+from autocomplete.models import AutocompleteInstrument
+
+
+class AutocompleteInstrument(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = AutocompleteInstrument.objects.all()
+
+        if self.q:
+            qs = qs.filter(name__istartswith=self.q)
+
+        return qs
+
+    def has_add_permission(self, request):
+        return True
