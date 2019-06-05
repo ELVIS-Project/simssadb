ae330b0c0ae6b8abee1119bfe894135942965112
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-06-05 14:46:46 -0400

    Formatting

--- database/views/search.py
+++ database/views/search.py
@@ -14,15 +14,21 @@ from database.utils.view_utils import make_summary_dict
 
 
 class SearchView(FormView):
-    facets = ['sacred_or_secular', 'instruments',
-              'composers', 'types', 'styles',
-              'certainty', 'file_format']
-    template_name = 'search/search.html'
+    facets = [
+        "sacred_or_secular",
+        "instruments",
+        "composers",
+        "types",
+        "styles",
+        "certainty",
+        "file_format",
+    ]
+    template_name = "search/search.html"
     search_queryset = SearchQuerySet().models(SymbolicMusicFile).all()
     queryset = None
     feature_types = FeatureType.objects.exclude(dimensions__gt=1)
-    codes = feature_types.values_list('code', flat=True)
-    summary_fields = ['file_type', 'file_size', 'source']
+    codes = feature_types.values_list("code", flat=True)
+    summary_fields = ["file_type", "file_size", "source"]
 
     @staticmethod
     def faceted_search(facets, query, search_queryset, request) -> set:
@@ -53,7 +59,7 @@ class SearchView(FormView):
         for facet in facets:
             value = request.GET.getlist(facet)
             if value:
-                key = facet + '__in'  # Add __in to filter the queryset
+                key = facet + "__in"  # Add __in to filter the queryset
                 key_value_pair = {key: value}
                 kwargs.update(key_value_pair)
 
@@ -64,82 +70,83 @@ class SearchView(FormView):
         # To return we take all the primary keys of the search_queryset,
         # convert them into ints and then make a set out of them (so there are
         # no repetitions)
-        return set(map(lambda x: int(x),
-                       (search_queryset.values_list('pk', flat=True))))
+        return set(
+            map(lambda x: int(x), (search_queryset.values_list("pk", flat=True)))
+        )
 
     @staticmethod
     def content_search(request, codes):
-
         def single_feature_search(code, min_val, max_val):
-            file_ids = list(ExtractedFeature.objects.filter(
+            file_ids = list(
+                ExtractedFeature.objects.filter(
                     instance_of_feature__code=code,
                     value__0__gte=min_val,
-                    value__0__lte=max_val
-                    ).values_list('feature_of_id', flat=True))
+                    value__0__lte=max_val,
+                )
+                .order_by("name")
+                .values_list("feature_of_id", flat=True)
+            )
             return file_ids
 
-        file_id_set = set(SymbolicMusicFile.objects.values_list('id',
-                                                                flat=True))
+        file_id_set = set(SymbolicMusicFile.objects.values_list("id", flat=True))
 
         if any(key in codes for key in request.GET):
             for key, value in request.GET.lists():
                 if key in codes:
-                    min_value, max_value = value[0].split(',')
-                    single_feature_results = single_feature_search(key,
-                                                                   min_value,
-                                                                   max_value)
-                    file_id_set = file_id_set.intersection(
-                            set(single_feature_results))
+                    min_value, max_value = value[0].split(",")
+                    single_feature_results = single_feature_search(
+                        key, min_value, max_value
+                    )
+                    file_id_set = file_id_set.intersection(set(single_feature_results))
         return file_id_set
 
     # TODO: make this more robust, specially when no query
     # TODO: add validation
     def get(self, request, *args, **kwargs):
-        query = request.GET['q']
+        query = request.GET["q"]
 
         faceted_search_results = self.faceted_search(
-                facets=self.facets,
-                query=query,
-                search_queryset=self.search_queryset,
-                request=request
-                )
+            facets=self.facets,
+            query=query,
+            search_queryset=self.search_queryset,
+            request=request,
+        )
         content_search_results = self.content_search(request, self.codes)
 
-        merged_result_ids = faceted_search_results.intersection(
-                content_search_results)
+        merged_result_ids = faceted_search_results.intersection(content_search_results)
 
-        files_queryset = SymbolicMusicFile.objects.filter(
-                id__in=merged_result_ids)
+        files_queryset = SymbolicMusicFile.objects.filter(id__in=merged_result_ids)
         files = []
 
         for file in files_queryset:
             new_element = make_summary_dict(file, self.summary_fields)
-            new_element['display'] = file.musical_work.display_name
-            new_element['file'] = file.display_name
+            new_element["display"] = file.musical_work.display_name
+            new_element["file"] = file.display_name
             files.append(new_element)
 
         ids_for_sqs = list(map(lambda x: str(x), merged_result_ids))
 
-        self.search_queryset = SearchQuerySet().models(
-                SymbolicMusicFile).filter(django_id__in=ids_for_sqs)
+        self.search_queryset = (
+            SearchQuerySet().models(SymbolicMusicFile).filter(django_id__in=ids_for_sqs)
+        )
 
         context = {
-            'content_search_form': ContentSearchForm(
-                    feature_types=self.feature_types,
-                    file_ids=ids_for_sqs,
-                    data=request.GET),
-            'faceted_search_form': FacetedSearchForm(
-                    selected_facets=self.facets,
-                    search_queryset=self.search_queryset,
-                    data=request.GET),
-            }
+            "content_search_form": ContentSearchForm(
+                feature_types=self.feature_types, file_ids=ids_for_sqs, data=request.GET
+            ),
+            "faceted_search_form": FacetedSearchForm(
+                selected_facets=self.facets,
+                search_queryset=self.search_queryset,
+                data=request.GET,
+            ),
+        }
 
         search_results = {
-            'list':        files,
-            'model_name':  'Files',
-            'model_count': files_queryset.count(),
-            'query':       query
-            }
+            "list": files,
+            "model_name": "Files",
+            "model_count": files_queryset.count(),
+            "query": query,
+        }
 
         context.update(search_results)
 
