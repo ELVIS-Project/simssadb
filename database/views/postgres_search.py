a0dbab34c2e8359be4e4b267f7fbb218b4d5e69d
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-06-12 13:10:56 -0400

    Override get_queryset() method so performs a full text search
    Refs #293

--- database/views/postgres_search.py
+++ database/views/postgres_search.py
@@ -3,3 +3,17 @@ class PostgresSearchView(ListView):
     model = MusicalWork
     context_object_name = "works"
     template_name = "search/pg_search.html"
+
+    def get_queryset(self):
+        query_string = self.request.GET.get("q")
+        if not query_string:
+            return MusicalWork.objects.none()
+        query = SearchQuery(query_string)
+        rank_annotation = SearchRank(F("search_document"), query)
+        query_set = (
+            MusicalWork.objects.annotate(rank=rank_annotation)
+            .filter(search_document=query)
+            .order_by("-rank")
+        )
+        return query_set
+
