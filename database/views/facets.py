8dd4d002c30ffb9832b9e05a24595cca5ff8255e
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-07-04 16:33:26 -0400

    Imports

--- database/views/facets.py
+++ database/views/facets.py
@@ -1,1 +1,15 @@
+from typing import List, Optional
+from abc import ABCMeta, abstractmethod
+from django.db.models import Count, F, Q, QuerySet, Value, When
+from database.models import (
+    ExtractedFeature,
+    FeatureType,
+    GenreAsInStyle,
+    GenreAsInType,
+    Instrument,
+    MusicalWork,
+    Person,
+    Section,
+    SymbolicMusicFile,
+)
 
