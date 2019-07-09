43b815c1ec55b7b123104f213507353b231cc4b2
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-07-09 14:57:24 -0400

    New clean_range() method

--- database/utils/model_utils.py
+++ database/utils/model_utils.py
@@ -1,1 +1,16 @@
 """A collection of functions that are useful for models"""
+from psycopg2.extras import NumericRange
+
+
+def clean_range(year_range: NumericRange) -> str:
+    if year_range.lower is not None and year_range.upper is not None:
+        if year_range.lower == year_range.upper:
+            return str(year_range.lower)
+        else:
+            return "(" + str(year_range.lower) + "-" + str(year_range.upper) + ")"
+    elif year_range.upper is None and year_range.lower is not None:
+        return str(year_range.lower)
+    elif year_range.lower is None and year_range.upper is not None:
+        return str(year_range.upper)
+    else:
+        return ""
