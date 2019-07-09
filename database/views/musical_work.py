203148b31cc553ea41f14e41ca866c49c501c139
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-07-09 14:57:40 -0400

    New MusicalWork detail view

---
+++ database/views/musical_work.py
@@ -0,0 +1,15 @@
+from django.views.generic import DetailView
+from database.models import MusicalWork
+from typing import Dict
+
+
+class MusicalWorkDetailView(DetailView):
+    model = MusicalWork
+    context_object_name = "musicalwork"
+    queryset = MusicalWork.objects.prefetch_related(
+        "genres_as_in_style",
+        "genres_as_in_type",
+        "sections__parts",
+        "parts__written_for",
+        "source_instantiations__manifested_by_files",
+    )
