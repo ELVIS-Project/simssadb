bb6d223bbef202b3cf48f5c12eeb422b510f3046
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-07-10 12:00:54 -0400

    Update __init__.py

--- database/views/__init__.py
+++ database/views/__init__.py
@@ -1,2 +1,14 @@
-from .postgres_search import PostgresSearchView
-from .front_end_views import *
+from database.views.postgres_search import PostgresSearchView
+from database.views.front_end_views import AboutView, HomeView, SignUpView
+
+from database.views.collection_of_sources import CollectionOfSourcesDetailView
+from database.views.file import FileDetailView
+from database.views.genre_as_in_style import GenreAsInStyleDetailView
+from database.views.genre_as_in_type import GenreAsInTypeDetailView
+from database.views.geographic_area import GeographicAreaDetailView
+from database.views.instrument import InstrumentDetailView
+from database.views.musical_work import MusicalWorkDetailView
+from database.views.part import PartDetailView
+from database.views.person import PersonDetailView
+from database.views.section import SectionDetailView
+from database.views.source import SourceDetailView
