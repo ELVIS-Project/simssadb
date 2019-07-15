73d208b2685cdd3054b26146ebdc506fc322d3a2
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-07-15 16:46:42 -0400

    delete outdated documentation

--- database/models/source_instantiation.py
+++ database/models/source_instantiation.py
@@ -6,51 +6,6 @@ from database.models import CustomBaseModel
 
 
 class SourceInstantiation(CustomBaseModel):
-    """An abstract entity defined by the music specified by a
-    particular Source, which corresponds to a particular
-    instantiation of all or part of a Musical Work, Sections or Parts.
-
-    Manifested by Audio, Symbolic, Text or Image files.
-
-    Attributes
-    ----------
-    SourceInstantiation.source : models.ForeignKey
-        Reference to the Source instantiated by this SourceInstantiation
-
-    SourceInstantiation.work : models.ForeignKey
-        Reference to a MusicalWork defined in full by this SourceInstantiation
-
-    SourceInstantiation.sections : models.ManyToManyField
-        References to Sections defined in full by this SourceInstantiation
-
-    SourceInstantiation.parts : models.ManyToManyField
-        References to Parts defined in full by this SourceInstantiation
-
-    SourceInstantiation.manifested_by_audio_files : models.ManyToOneRel
-        References to AudioFiles that manifest this SourceInstantiation
-
-    SourceInstantiation.manifested_by_text_files : models.ManyToOneRel
-        References to TextFiles that manifest this SourceInstantiation
-
-    SourceInstantiation.manifested_by_image_files : models.ManyToOneRel
-        References to ImageFiles that manifest this SourceInstantiation
-
-    SourceInstantiation.manifested_by_sym_files : models.ManyToOneRel
-        References to SymbolicMusicFiles that manifest this SourceInstantiation
-
-    See Also
-    --------
-    database.models.CustomBaseModel
-    database.models.Source
-    database.models.SymbolicMusicFile
-    database.models.TextFile
-    database.models.ImageFile
-    database.models.AudioFile
-    database.models.MusicalWork
-    database.models.Section
-    database.models.Part
-    """
-
     source = models.ForeignKey(
         "Source",
         null=False,
