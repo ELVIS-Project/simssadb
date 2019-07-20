2e3d5ee4ffe427f115fbd51776e03a8eb343741f
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-07-20 13:23:33 -0400

    create MusicalWorkListView

--- database/views/musical_work.py
+++ database/views/musical_work.py
@@ -1,13 +1,23 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from database.models import MusicalWork
-from typing import Dict
-from django.urls import reverse
-from django.db.models import F
+from extra_views import SearchableListMixin
 
 
 class MusicalWorkDetailView(DetailView):
     model = MusicalWork
     context_object_name = "musicalwork"
     queryset = MusicalWork.objects.prefetch_related(
-        "parts__written_for", "sections__parts__written_for",
+        "parts__written_for", "sections__parts__written_for"
     )
+
+
+class MusicalWorkListView(SearchableListMixin, ListView):
+    model = MusicalWork
+    search_fields = [
+        "contributions__person__surname",
+        "variant_titles",
+        "contributions__person__given_name",
+    ]
+    context_object_name = "musicalworks"
+    queryset = MusicalWork.objects.order_by("variant_titles")
+    paginate_by = 100
