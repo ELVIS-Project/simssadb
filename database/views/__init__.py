d2d29454af24d28eed414e4eb3e570a506c14d95
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-07-20 13:22:26 -0400

    add views to __init.py__

--- database/views/__init__.py
+++ database/views/__init__.py
@@ -7,8 +7,8 @@ from database.views.genre_as_in_style import GenreAsInStyleDetailView
 from database.views.genre_as_in_type import GenreAsInTypeDetailView
 from database.views.geographic_area import GeographicAreaDetailView
 from database.views.instrument import InstrumentDetailView
-from database.views.musical_work import MusicalWorkDetailView
+from database.views.musical_work import MusicalWorkDetailView, MusicalWorkListView
 from database.views.part import PartDetailView
-from database.views.person import PersonDetailView
+from database.views.person import PersonDetailView, PersonListView
 from database.views.section import SectionDetailView
 from database.views.source import SourceDetailView
