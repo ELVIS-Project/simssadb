b6634fc8c0186dca36f53e348e7805ab183ae80a
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-07-24 15:56:59 -0400

    fix clean_range function
    now it returns an empty string if no year_range passed

--- database/utils/model_utils.py
+++ database/utils/model_utils.py
@@ -3,14 +3,17 @@ from psycopg2.extras import NumericRange
 
 
 def clean_range(year_range: NumericRange) -> str:
-    if year_range.lower is not None and year_range.upper is not None:
-        if year_range.lower == year_range.upper:
+    if year_range:
+        if year_range.lower is not None and year_range.upper is not None:
+            if year_range.lower == year_range.upper:
+                return str(year_range.lower)
+            else:
+                return "(" + str(year_range.lower) + "-" + str(year_range.upper) + ")"
+        elif year_range.upper is None and year_range.lower is not None:
             return str(year_range.lower)
+        elif year_range.lower is None and year_range.upper is not None:
+            return str(year_range.upper)
         else:
-            return "(" + str(year_range.lower) + "-" + str(year_range.upper) + ")"
-    elif year_range.upper is None and year_range.lower is not None:
-        return str(year_range.lower)
-    elif year_range.lower is None and year_range.upper is not None:
-        return str(year_range.upper)
+            return ""
     else:
         return ""
