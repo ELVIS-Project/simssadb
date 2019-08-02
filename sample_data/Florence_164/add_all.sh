908078e1d8904c43b5de94b4f0f7979a58072a61
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2019-08-02 16:02:22 -0400

    These files are updated and need to be tracked

---
+++ sample_data/Florence_164/add_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python software_adder.py
+python instrument_adder.py
+python genre_adder.py
+python person_adder.py
+python collection_adder.py
+python work_source_adder.py
+python location_adder.py
