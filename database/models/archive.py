b0a0219986a086adf7e3589bbda26ed91dd02abc
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-09-25 14:30:14 -0400

    update archive model documentation

--- database/models/archive.py
+++ database/models/archive.py
@@ -1,27 +1,25 @@
-"""Define an Archive model"""
+"""Defines the Archive model"""
 from django.db import models
-
 from database.models.custom_base_model import CustomBaseModel
 
 
 class Archive(CustomBaseModel):
-    """A location where Sources are stored.
-
-    e.g: A database or a library.
-
+    """An Archive (physical or digital) of Sources
+    
     Attributes
     ----------
-    Archive.name : models.CharField
-        The name of the archive.
+    name: models.CharField
+        The name of the Archive
 
-    See Also
-    --------
-    database.models.CustomBaseModel
-    database.models.Sources
+    url: models.URLField
+        The URL of this Archive
+    
+    sources: models.ManyToManyField
+        Many to many reference to Sources that belong to this Archive
     """
 
     name = models.CharField(
-        max_length=200, blank=False, null=False, help_text="The name of the Archive"
+        max_length=200, blank=False, null=False, help_text="The name of this Archive"
     )
     url = models.URLField(blank=True, null=True, help_text="The URL of the Archive")
     sources = models.ManyToManyField(
