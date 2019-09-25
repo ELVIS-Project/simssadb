8658ce2b98c0cf9a4e659b58c239cea9bf37fa7c
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-09-25 13:32:52 -0400

    add AJAX views

--- database/views/research_corpus.py
+++ database/views/research_corpus.py
@@ -1,7 +1,8 @@
-from django.views.generic import DetailView, ListView
-from database.models import ResearchCorpus
+from django.views.generic import DetailView, ListView, View, TemplateView
+from database.models import ResearchCorpus, File
 from extra_views import SearchableListMixin
-
+from django.http import JsonResponse
+import json
 
 class ResearchCorpusDetailView(DetailView):
     model = ResearchCorpus
@@ -14,3 +15,20 @@ class ResearchCorpusListView(SearchableListMixin, ListView):
     queryset = ResearchCorpus.objects.order_by("title")
     paginate_by = 100
     template_name = "list.html"
+
+
+class AddFileToResearchCorpus(View):
+    def get(self, request, *args, **kwargs):
+        print("Get request!")
+        return JsonResponse({"hello": "world"})
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        corpus = ResearchCorpus.objects.get(id=data["research_corpus"])
+        file = File.objects.get(id=data["file"])
+        corpus.files.add(file)
+        response = {"file_name": file.__str__(), "research_corpus": corpus.__str__(), }
+        return JsonResponse(response)
+
+class ButtonTestView(TemplateView):
+    template_name = "button_test.html"
