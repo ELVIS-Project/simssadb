824b8d8e108ba17944f66c053c12efeeb837b65b
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2019-10-28 13:58:45 -0400

    add documentation to validation workflow model

--- database/models/validation_workflow.py
+++ database/models/validation_workflow.py
@@ -4,7 +4,33 @@ from database.models.custom_base_model import CustomBaseModel
 
 
 class ValidationWorkFlow(CustomBaseModel):
-    persons = models.CharField(
+    """A workflow used to validate a symbolic music file.
+
+    The ValidationWorkFlow model stores information on how a symbolic music file was
+    validated to aid with reproducibility and provenance tracking.
+
+    Attributes
+    ----------
+    validator_names: models.CharField
+        The names of the persons that validated a file 
+        This is simply a string and does **not** reference the Person model
+    
+    validator_software: models.ForeignKey
+        A reference to the Software used to validate a file
+    
+    file: models.ForeignKey
+        A reference to the file that was validated using this workflow
+    
+    notes: models.TextField
+        Any extra notes or remarks the user wishes to provide
+    
+    workflow_file: models.FileField
+        A configuration file that defines a software validation workflow
+    
+    workflow_text: models.TextField
+        A textual description of the workflow used to validate a file
+    """
+    validator_names = models.CharField(
         max_length=512,
         blank=True,
         null=True,
