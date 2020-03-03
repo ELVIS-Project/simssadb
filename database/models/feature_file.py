d8a87600d3bd84ea65b80efcec7c4cc1f64757f9
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2020-03-03 15:34:15 -0500

    fix path for feature_files

--- database/models/feature_file.py
+++ database/models/feature_file.py
@@ -32,17 +32,17 @@ class FeatureFile(CustomBaseModel):
         max_length=100, help_text="The format of the FeatureFile"
     )
     file = models.FileField(
-        upload_to="user_files/feature_files",
+        upload_to="user_files/extracted_features",
         max_length=255,
         help_text="The actual feature file"
     )
     config_file = models.FileField(
-        upload_to="user_files/feature_files",
+        upload_to="user_files/extracted_features",
         max_length=255,
         help_text="A file describing the configuration used to extract the features",
     )
     feature_definition_file = models.FileField(
-        upload_to="user_files/feature_files",
+        upload_to="user_files/extracted_features",
         max_length=255,
         help_text="A file that defines the features represented in the FeatureFile",
     )
