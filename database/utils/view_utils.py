bc1fcde2625cbaf4f3284b03d7d845b450d5594c
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2020-03-03 15:29:31 -0500

    add option to include feature files when zipping
    refs #370

--- database/utils/view_utils.py
+++ database/utils/view_utils.py
@@ -2,11 +2,14 @@ import zipfile
 import io
 import os
 from django.db.models import QuerySet
+from typing import Optional
 from database.models.file import File
 from database.models.feature_file import FeatureFile
 
 
-def zip_files(files: QuerySet, zipfile_name: str) -> io.BytesIO:
+def zip_files(files: QuerySet,
+              zipfile_name: str,
+              feature_files: Optional[QuerySet] = None) -> io.BytesIO:
     """Zips all files in a QuerySet of files without writing to disk
 
     Takes all the files in a QuerySet of files and zips them in memory, 
@@ -16,9 +19,11 @@ def zip_files(files: QuerySet, zipfile_name: str) -> io.BytesIO:
     Parameters
     ----------
     files : QuerySet
-        A QuerySet of files (either content files or feature files)
+        A QuerySet of Files
     zipfile_name : str
         The name of the final zip file
+    feature_files: Optional[QuerySet]
+        An optional QuerySet of Feature Files that are describe the features of the Files
 
     Raises
     ------
@@ -30,8 +35,8 @@ def zip_files(files: QuerySet, zipfile_name: str) -> io.BytesIO:
         An in-memory buffer with the zipfile. Should be closed by caller.
     """
     model_name = files.model.__name__
-    if model_name != "File" and model_name != "FeatureFile":
-        raise TypeError("The QuerySet model is not File or FeatureFile")
+    if model_name != "File":
+        raise TypeError("The QuerySet model is not File")
 
     in_memory = io.BytesIO()
     zip_file = zipfile.ZipFile(in_memory, mode="w")
@@ -41,6 +46,14 @@ def zip_files(files: QuerySet, zipfile_name: str) -> io.BytesIO:
         file_dir, file_name = os.path.split(file.file.url)
         zip_path = os.path.join(zip_subdir, file_name)
         zip_file.write(file.file.url, zip_path)
+
+    if feature_files is not None:
+        if feature_files.model.__name__ != "FeatureFile":
+            raise TypeError("The QuerySet model is not FeatureFile")
+        for feature_file in feature_files:
+            file_dir, feature_file_name = os.path.split(feature_file.file.name)
+            zip_path = os.path.join(zip_subdir, feature_file_name)
+            zip_file.write(feature_file.file.name, zip_path)
     zip_file.close()
 
     return in_memory
