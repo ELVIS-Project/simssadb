96e794eb503dc945f56a27352e652bd2a6b3f262
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2020-03-03 15:32:57 -0500

    include request argument on download_file function

--- database/views/download.py
+++ database/views/download.py
@@ -7,7 +7,9 @@ from django.http import FileResponse, HttpResponse
 from django.shortcuts import get_object_or_404
 from database.utils.view_utils import zip_files
 
-def download_file(is_content: bool, pk: int) -> FileResponse:
+
+@require_safe
+def download_file(request, is_content: bool, pk: int) -> FileResponse:
     if is_content:
         file_object = get_object_or_404(File, pk=pk)
     else:
@@ -15,11 +17,15 @@ def download_file(is_content: bool, pk: int) -> FileResponse:
     response = FileResponse(file_object.file, as_attachment=True)
     return response
 
-def download_content_file(request, pk: int) -> FileResponse:
-    return download_file(is_content=True, pk=pk)
 
+@require_safe
+def download_content_file(request, pk: int) -> FileResponse:
+    return download_file(request, is_content=True, pk=pk)
+
+
+@require_safe
 def download_feature_file(request, pk: int) -> FileResponse:
-    return download_file(is_content=False, pk=pk)
+    return download_file(request, is_content=False, pk=pk)
 
     response = HttpResponse(zip_file.getvalue(), content_type="application/zip")
     response["Content-Disposition"] = f"attachment; filename={research_corpus.title}.zip"
