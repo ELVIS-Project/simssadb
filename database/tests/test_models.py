0dc67d417b0467643c92deaa49c882b16069743f
Gustavo Polins Pedro <gustavo.polinspedro@mail.mcgill.ca>
2020-03-17 17:32:14 -0400

    create archive model tests

--- database/tests/test_models.py
+++ database/tests/test_models.py
@@ -1,3 +1,20 @@
+import os
 from django.test import TestCase
+from django.core.files import File as PythonFile
+from django.conf import settings
+from database.models import *
 
 # Create your tests here.
+
+class ArchiveModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Archive.objects.create(name="Test Archive", url="https://testarchive.com")
+
+    def test_str(self) -> None:
+        archive = Archive.objects.first()
+        self.assertEqual(str(archive), "Test Archive")
+
+    def test_get_absolute_url(self) -> None:
+        archive = Archive.objects.get(id=1)
+        self.assertEquals(archive.get_absolute_url(), f"/archives/{archive.id}")
