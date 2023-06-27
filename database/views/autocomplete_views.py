9d3ef29217b5707561dc6247510068c530f36b3a
rebmizrahi <becca.mizrahi@gmail.com>
2023-06-27 10:28:58 -0400

    Adds musical work autocomplete widget

--- database/views/autocomplete_views.py
+++ database/views/autocomplete_views.py
@@ -1,7 +1,7 @@
 from dal import autocomplete
-
+from django.db.models import Q
 from database.models import GenreAsInStyle, GenreAsInType, GeographicArea, \
-    Instrument, Software, Archive, File
+    Instrument, Software, Archive, File, MusicalWork
 
 
 class StyleAutocomplete(autocomplete.Select2QuerySetView):
@@ -91,3 +91,37 @@ class ArchiveAutocomplete(autocomplete.Select2QuerySetView):
 
     def has_add_permission(self, request):
         return True
+
+
+class MusicalWorkAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = MusicalWork.objects.all()
+
+        if self.q:
+            qs = qs.filter(Q(contributors__given_name__icontains=self.q) 
+                           | Q(contributors__surname__icontains=self.q)
+                           | Q(variant_titles__icontains=self.q))
+
+        return qs
+
+    def has_add_permission(self, request):
+        return True
+    
+    def get_results(self, context):
+        results = []
+
+        for musical_work in context["object_list"]:
+            result = {
+                "id": str(musical_work.pk),
+                "text": musical_work.variant_titles[0],  # First element of variant_titles
+                "selected_text": musical_work.variant_titles[0],  # First element of variant_titles
+            }
+
+            if musical_work.contributors.exists():
+                first_contributor = musical_work.contributors.first()
+                result["text"] += " - " + str(first_contributor)  # Append first contributor
+                result["selected_text"] += " - " + str(first_contributor)  # Append first contributor
+
+            results.append(result)
+
+        return results
